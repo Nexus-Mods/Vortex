7753977e646d4829045779caaadf75e205d5926e
TanninOne <sherb@gmx.net>
2016-11-30 09:57:52 +0100

    sligth fixes

--- src/extensions/savegame_management/actions/session.ts
+++ src/extensions/savegame_management/actions/session.ts
@@ -2,19 +2,22 @@ import { createAction } from 'redux-act';
 
 import { ISavegame, SavegameState } from '../types/ISavegame';
 
-export const addSavegame = createAction('ADD_SAVEGAME',
+export const addSavegame: any = createAction('ADD_SAVEGAME',
   (save: ISavegame) => save);
 
-export const setSavegameState = createAction('SET_SAVEGAME_STATE',
+export const setSavegameState: any = createAction('SET_SAVEGAME_STATE',
   (id: string, savegameState: SavegameState) => ({ id, savegameState }));
 
-export const setSavegameAttribute = createAction('SET_SAVEGAME_ATTRIBUTE',
+export const setSavegameAttribute: any = createAction('SET_SAVEGAME_ATTRIBUTE',
   (id: string, attribute: string, value: any) => ({ id, attribute, value }));
 
-export const clearSavegames = createAction('CLEAR_SAVEGAMES');
+export const clearSavegames: any = createAction('CLEAR_SAVEGAMES');
 
-export const setSavegamelistAttributeVisible = createAction('SET_SAVEGAMELIST_ATTRIBUTE_VISIBLE',
-  (attributeId: string, visible: boolean) => { return { attributeId, visible }; });
+export const setSavegamelistAttributeVisible: any =
+    createAction('SET_SAVEGAMELIST_ATTRIBUTE_VISIBLE',
+                 (attributeId: string, visible: boolean) => {
+                   return {attributeId, visible};
+                 });
 
-export const setSavegamelistAttributeSort = createAction('SET_SAVEGAMELIST_ATTRIBUTE_SORT',
+export const setSavegamelistAttributeSort: any = createAction('SET_SAVEGAMELIST_ATTRIBUTE_SORT',
   (attributeId: string, direction: string) => { return { attributeId, direction }; });
