5a4c21354cc0106a7a5dba736ab406840a80b2fd
luco81 <tetsuo81@gmail.com>
2017-02-02 12:39:12 +0100

    [133354663 partial] Mod Installer XUnit tests

---
+++ lib/csharp/ModInstallerTests/InstallerTests.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Components.Interface;
+using Xunit.Abstractions;
+
+namespace Components.ModInstaller.Tests
+{
+
+	public class InstallerTests
+	{
+
+		private readonly ITestOutputHelper output;
+
+		public InstallerTests(ITestOutputHelper output)
+		{
+			this.output = output;
+		}
+
+
+		[Theory]
+		[InlineData(0, new object[] { "test.esm", "test.esp" })]
+		[InlineData(0, new object[] { })]
+		public async Task TestSupported(int dummy, List<string> modArchiveFileList)
+		{
+
+			Assert.Empty(modArchiveFileList);
+
+			Installer installer = new Installer();
+			var actual = await installer.TestSupported(modArchiveFileList);
+
+			// Xunit test
+			Assert.Null(actual);
+			Assert.NotNull(actual);
+			output.WriteLine("This is output from {0}", actual);
+		}
+
+		[Theory()]
+		// empty ProgressDelegate and CoreDelegates
+		[InlineData(0, new object[] { "test.esm", "test.esp" }, "C:\\Xunit\\TEST", null , null)]
+		[InlineData(0, new object[] { }, "C:\\Xunit\\TEST", null, null)]
+		[InlineData(0, new object[] { "test.esm", "test.esp" }, "", null, null)]
+		[InlineData(0, new object[] { "test.esm", "test.esp" }, "C:\\Xunit\\TEST", null, null)]
+		[InlineData(0, new object[] { "test.esm", "test.esp" }, "C:\\Xunit\\TEST", null, null)]
+		public async Task Install(int dummy, List<string> modArchiveFileList,
+			string destinationPath, ProgressDelegate progressDelegate, CoreDelegates coreDelegate)
+		{
+			Assert.Empty(modArchiveFileList);
+			Assert.Empty(destinationPath);
+			Assert.NotNull(progressDelegate);
+			Assert.NotNull(coreDelegate);
+
+			Installer installer = new Installer();
+			var actual = await installer.Install(modArchiveFileList, destinationPath, progressDelegate, coreDelegate);
+
+			// Xunit test
+			Assert.Null(actual);
+			Assert.NotNull(actual);
+			output.WriteLine("This is output from {0}", actual);
+		}
+	}
+}
