b315d878364eca1dccface976ab3ffcf343d515c
luco81 <tetsuo81@gmail.com>
2017-03-17 09:16:27 +0100

    Settings Metaserver tests

---
+++ __tests__/actions.settings_metaserver.test.js
@@ -0,0 +1,19 @@
+import * as actions from '../src/extensions/settings_metaserver/actions';
+
+describe('addMetaserver', () => {
+  it('creates the correct action', () => {
+    expect(actions.addMetaserver('id1', 'url1')).toEqual({
+      type: 'ADD_METASERVER',
+      payload: { id: 'id1', url: 'url1' },
+    });
+  });
+});
+
+describe('removeMetaserver', () => {
+  it('creates the correct action', () => {
+    expect(actions.removeMetaserver('id1', 86400)).toEqual({
+      type: 'REMOVE_METASERVER',
+      payload: { id: 'id1', cacheDurationSec: 86400 },
+    });
+  });
+});
---
+++ __tests__/reducers.settings_metaserver.test.js
@@ -0,0 +1,17 @@
+import settingsReducer from '../src/extensions/settings_metaserver/reducers';
+
+describe('addMetaserver', () => {
+  it('add the Metaserver', () => {
+    let input = { 'servers': {id1: { url1: 'url', cacheDurationSec: 86400, priority: 1 } }};
+    let result = settingsReducer.reducers.ADD_METASERVER(input, { id: 'id1', url: 'new url' });
+    expect(result).toEqual({ 'servers': {id1: { url: 'new url', cacheDurationSec: 86400, priority: 1 } }});
+  });
+});
+
+describe('removeMetaserver', () => {
+  it('remove the Metaserver', () => {
+    let input = { 'servers': {id1: {} } };
+    let result = settingsReducer.reducers.REMOVE_METASERVER(input, { id: 'id1', cacheDurationSec: 86400 });
+    expect(result).toEqual({'servers': {} });
+  });
+});
