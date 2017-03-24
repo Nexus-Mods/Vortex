0f0aeb4d74acc4fd8e222d03129257a25e47a48e
TanninOne <sherb@gmx.net>
2017-03-24 14:14:31 +0100

    fixed bugs with error handling during mod install
    fixes issue #17

--- src/extensions/mod_management/types/IInstallContext.ts
+++ src/extensions/mod_management/types/IInstallContext.ts
@@ -1,9 +1,11 @@
+export type InstallOutcome = 'success' | 'failed' | 'canceled';
+
 export interface IInstallContext {
   startIndicator: (id: string)  => void;
-  stopIndicator: (id: string) => void;
+  stopIndicator: () => void;
   startInstallCB: (id: string, archivePath: string, destinationPath: string) =>
       void;
-  finishInstallCB: (id: string, success: boolean) => void;
+  finishInstallCB: (success: InstallOutcome, info?: any) => void;
   progressCB: (percent: number, file: string) => void;
   reportError: (message: string, details?: string) => void;
 }
