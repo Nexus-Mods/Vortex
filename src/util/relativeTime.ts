6c9568b64cc54e13681c07ca98b409abd9a35189
luco81 <tetsuo81@gmail.com>
2017-04-18 09:53:39 +0200

    Polished relativeTime code

--- src/util/relativeTime.ts
+++ src/util/relativeTime.ts
@@ -6,32 +6,31 @@ const SEC_PER_MONTH = SEC_PER_DAY * 30;
 const SEC_PER_YEAR = SEC_PER_DAY * 365;
 
 function relativeTime(date: Date, t: I18next.TranslationFunction): string {
-  const deltaSec = (new Date().getTime() - date.getTime()) / 1000;
+  let deltaSec = (new Date().getTime() - date.getTime()) / 1000;
+
+  if (isNaN(deltaSec)) {
+    deltaSec = 0;
+  }
+
   if (deltaSec < SEC_PER_MIN) {
     return t('seconds ago');
   } else if (deltaSec < SEC_PER_HOUR) {
-    const count = !isNaN(Math.floor(deltaSec / SEC_PER_MIN)) ?
-      Math.floor(deltaSec / SEC_PER_MIN) : 0;
+    const count = Math.floor(deltaSec / SEC_PER_MIN);
     return t('{{ count }} minute ago', { count });
   } else if (deltaSec < SEC_PER_DAY) {
-    const count = !isNaN(Math.floor(deltaSec / SEC_PER_HOUR)) ?
-      Math.floor(deltaSec / SEC_PER_HOUR) : 0;
+    const count = Math.floor(deltaSec / SEC_PER_HOUR);
     return t('{{ count }} hour ago', { count });
   } else if (deltaSec < SEC_PER_WEEK) {
-    const count = !isNaN(Math.floor(deltaSec / SEC_PER_DAY)) ?
-      Math.floor(deltaSec / SEC_PER_DAY) : 0;
+    const count = Math.floor(deltaSec / SEC_PER_DAY);
     return t('{{ count }} day ago', { count });
   } else if (deltaSec < SEC_PER_MONTH) {
-    const count = !isNaN(Math.floor(deltaSec / SEC_PER_WEEK)) ?
-      Math.floor(deltaSec / SEC_PER_WEEK) : 0;
+    const count = Math.floor(deltaSec / SEC_PER_WEEK);
     return t('{{ count }} week ago', { count });
   } else if (deltaSec < SEC_PER_YEAR) {
-    const count = !isNaN(Math.floor(deltaSec / SEC_PER_MONTH)) ?
-      Math.floor(deltaSec / SEC_PER_MONTH) : 0;
+    const count = Math.floor(deltaSec / SEC_PER_MONTH);
     return t('{{ count }} month ago', { count });
   } else {
-    const count = !isNaN(Math.floor(deltaSec / SEC_PER_YEAR)) ?
-      Math.floor(deltaSec / SEC_PER_YEAR) : 0;
+    const count = Math.floor(deltaSec / SEC_PER_YEAR);
     return t('{{ count }} year ago', { count });
   }
 }
