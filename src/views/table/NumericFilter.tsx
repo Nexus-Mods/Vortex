cdc926082baadbc56f42e16cbcff60e30c027220
DuskDweller <fabio.auletta@gmail.com>
2017-04-20 12:56:53 +0200

    [#142697783 complete] Added higher/lower toggle to Numeric filter
    Added the higher/lower than toggle to the numeric filter.

--- src/views/table/NumericFilter.tsx
+++ src/views/table/NumericFilter.tsx
@@ -1,22 +1,60 @@
 import {IFilterProps, ITableFilter} from '../../types/ITableAttribute';
 
+import { IconButton } from '../../views/TooltipControls';
+
 import * as React from 'react';
-import { FormControl } from 'react-bootstrap';
+import { FormControl, InputGroup } from 'react-bootstrap';
+
+let comparisonHigherThan: boolean = true;
 
 export class NumericFilterComponent extends React.Component<IFilterProps, {}> {
+  private lastAttributeId;
+  private lastValue;
+
   public render(): JSX.Element {
-    const { filter } = this.props;
-    return <FormControl
-      className='form-field-compact'
-      type='number'
-      value={filter || ''}
-      onChange={this.changeFilter}
-    />;
+    const { filter, t } = this.props;
+    let directionIcon;
+    let tooltip = t('Toggle higher/lower than');
+
+    if (comparisonHigherThan) {
+      directionIcon = 'long-arrow-down';
+    } else {
+      directionIcon = 'long-arrow-up';
+    }
+
+    return (
+      <InputGroup>
+        <FormControl
+          className='form-field-compact'
+          type='number'
+          value={filter || ''}
+          onChange={this.changeFilter}
+        />
+        <InputGroup.Button>
+          <IconButton
+            className='btn-embed'
+            id='toggleDate'
+            tooltip={tooltip}
+            icon={directionIcon}
+            onClick={this.toggleDirection}
+          />
+        </InputGroup.Button>
+      </InputGroup>
+    );
   }
 
   private changeFilter = (evt) => {
     const { attributeId, onSetFilter } = this.props;
     onSetFilter(attributeId, evt.currentTarget.value);
+  }
+
+    private toggleDirection = (evt) => {
+    if (this.lastValue === undefined) {
+      return;
+    }
+    comparisonHigherThan = !comparisonHigherThan;
+    const { onSetFilter } = this.props;
+    onSetFilter(this.lastAttributeId, this.lastValue);
   }
 }
 
@@ -25,7 +63,7 @@ class NumericFilter implements ITableFilter {
   public raw = false;
 
   public matches(filter: any, value: any): boolean {
-      return (value >= filter);
+      return (comparisonHigherThan) ? (value >= filter) : (value <= filter);
   }
 }
 
