80f3a73899f2bae65ae0ae07fea3eb055991c643
TanninOne <sherb@gmx.net>
2017-06-02 12:43:58 +0200

    hardlink-activator now cleans up empty directories it created before

--- src/extensions/hardlink_activator/index.ts
+++ src/extensions/hardlink_activator/index.ts
@@ -16,6 +16,15 @@ import * as path from 'path';
 
 import * as util from 'util';
 
+const TAG_NAME = '__delete_if_empty';
+
+export class FileFound extends Error {
+  constructor(name) {
+    super(name);
+    this.name = this.constructor.name;
+  }
+}
+
 class ModActivator extends LinkingActivator {
   constructor(api: IExtensionApi) {
     super(
@@ -62,29 +71,61 @@ class ModActivator extends LinkingActivator {
 
   protected purgeLinks(installPath: string, dataPath: string): Promise<void> {
     const inos = new Set<number>();
+    const deleteIfEmpty: string[] = [];
 
-    return walk(installPath, (iterPath: string, stats: fs.Stats) => {
-      if (stats.nlink > 1) {
-        inos.add(stats.ino);
-      }
-      return Promise.resolve();
-    }).then(() => {
-      walk(dataPath, (iterPath: string, stats: fs.Stats) => {
-        if ((stats.nlink > 1) && (inos.has(stats.ino))) {
-          return fs.unlinkAsync(iterPath);
-        } else {
-          return Promise.resolve();
-        }
-      });
-    });
+    // find ids of all files in our mods directory
+    return walk(installPath,
+                (iterPath: string, stats: fs.Stats) => {
+                  if (stats.nlink > 1) {
+                    inos.add(stats.ino);
+                  }
+                  return Promise.resolve();
+                })
+        // now remove all files in the game directory that have the same id
+        .then(() => walk(dataPath,
+                         (iterPath: string, stats: fs.Stats) => {
+                           if (path.basename(iterPath) === TAG_NAME) {
+                             deleteIfEmpty.push(path.dirname(iterPath));
+                           }
+                           return ((stats.nlink > 1) && (inos.has(stats.ino))) ?
+                                      fs.unlinkAsync(iterPath) :
+                                      Promise.resolve();
+                         }))
+        .then(() => {
+          // sort by length descending
+          const sorted = deleteIfEmpty.sort((lhs: string, rhs: string) => rhs.length - lhs.length);
+          // go through the list and remove every dir that contains only the tag file.
+          // The list is sorted is longest path first so this should remove the deepest paths first
+          const findFiles = this.genFindFiles();
+          Promise.mapSeries(
+              sorted,
+              delPath => findFiles(delPath).then(
+                  hasFiles => hasFiles
+                                  ? Promise.resolve()
+                                  : fs.unlinkAsync(path.join(delPath, TAG_NAME))
+                                      .then(() => this.rmEmptyDir(delPath))));
+        });
   }
 
   protected linkFile(linkPath: string, sourcePath: string): Promise<void> {
     return fs.ensureDirAsync(path.dirname(linkPath))
-        .then(() => fs.linkAsync(sourcePath, linkPath)
-                        .catch((err) => {
-                          if (err.code !== 'EEXIST') { throw err; }
-                        }));
+        .then((created: any) => {
+          let tagDir: Promise<void>;
+          if (created !== null) {
+            tagDir = fs.writeFileAsync(
+                path.join(created, TAG_NAME),
+                'This directory was created by Vortex deployment and will be removed ' +
+                    'during purging if it\'s empty');
+          } else {
+            tagDir = Promise.resolve();
+          }
+          return tagDir.then(() => fs.linkAsync(sourcePath, linkPath))
+              .catch((err) => {
+                if (err.code !== 'EEXIST') {
+                  throw err;
+                }
+              });
+        });
   }
 
   protected unlinkFile(linkPath: string): Promise<void> {
@@ -101,6 +142,39 @@ class ModActivator extends LinkingActivator {
         );
       }
     });
+  }
+
+  private genFindFiles(): (dirPath: string) => Promise<boolean> {
+    const notEmpty = new Set<string>();
+
+    return (dirPath: string) => {
+      return walk(dirPath,
+                  (iterPath: string, stats: fsOrig.Stats) => {
+                    if (notEmpty.has(iterPath) ||
+                        (!stats.isDirectory() &&
+                         (path.basename(iterPath) !== TAG_NAME))) {
+                      return Promise.reject(new FileFound(iterPath));
+                    }
+                    return Promise.resolve();
+                  })
+          .then(() => Promise.resolve(false))
+          .catch(FileFound, err => {
+            notEmpty.add(dirPath);
+            return Promise.resolve(true);
+          });
+    };
+  }
+
+  private rmEmptyDir(dirPath: string): Promise<void> {
+    return fs.readdirAsync(dirPath)
+        .then((files: string[]) => Promise.map(
+                  files,
+                  file => {
+                    const fullPath = path.join(dirPath, file);
+                    return file === TAG_NAME ? fs.unlinkAsync(fullPath)
+                                             : this.rmEmptyDir(fullPath);
+                  }))
+        .then(() => fs.rmdirAsync(dirPath));
   }
 }
 
