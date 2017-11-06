ef89a625bb1d106ddb73b790c9323a5aed1e3207
TanninOne <sherb@gmx.net>
2017-11-06 18:49:32 +0100

    made the "external changes" dialog a bit friendlier
    I hope...

--- src/extensions/mod_management/views/ExternalChangeDialog.tsx
+++ src/extensions/mod_management/views/ExternalChangeDialog.tsx
@@ -1,16 +1,18 @@
 import Table from '../../../controls/Table';
+import Toggle from '../../../controls/Toggle';
 import { Button } from '../../../controls/TooltipControls';
 import { ITableAttribute } from '../../../types/ITableAttribute';
 import { ComponentEx, connect, translate } from '../../../util/ComponentEx';
-import { midClip } from '../../../util/util';
+import { midClip, setdefault } from '../../../util/util';
 
 import { confirmExternalChanges, setExternalChangeAction } from '../actions/externalChanges';
 
 import { FileAction, IFileEntry } from '../types/IFileEntry';
 
 import * as I18next from 'i18next';
+import * as update from 'immutability-helper';
 import * as React from 'react';
-import { Modal } from 'react-bootstrap';
+import { Collapse, Modal } from 'react-bootstrap';
 import * as Redux from 'redux';
 import * as ReduxThunk from 'redux-thunk';
 
@@ -35,25 +37,57 @@ const nop = () => undefined;
 
 const possibleActions: { [type: string]: IPossibleAction[] } = {
   refchange: [
-    { key: 'import', text: 'Save modified' },
-    { key: 'drop', text: 'Restore from mod' },
+    { key: 'import', text: 'Apply' },
+    { key: 'drop', text: 'Restore' },
   ],
   valchange: [
-    { key: 'keep', text: 'Keep modified' },
+    { key: 'nop', text: 'Apply' },
+    // TODO: implement a "restore from archive" option
   ],
   deleted: [
-    { key: 'restore', text: 'Restore from mod' },
-    { key: 'delete', text: 'Delete in mod as well' },
+    { key: 'delete', text: 'Apply' },
+    { key: 'restore', text: 'Restore' },
   ],
   srcdeleted: [
-    { key: 'drop', text: 'Delete in destination as well' },
-    { key: 'import', text: 'Import to mod' },
+    { key: 'drop', text: 'Apply' },
+    { key: 'import', text: 'Restore' },
   ],
 };
 
 type IProps = IBaseProps & IConnectedProps & IActionProps;
 
-class ExternalChangeDialog extends ComponentEx<IProps, { counter: number }> {
+interface ISourceMap {
+ [source: string]: IFileEntry[];
+}
+
+function bySource(prev: ISourceMap, value: IFileEntry): ISourceMap {
+  setdefault(prev, value.source, []).push(value);
+  return prev;
+}
+
+interface ISourceEntry {
+  id: string;
+  filePaths: string[];
+  action: FileAction;
+  type: 'refchange' | 'valchange' | 'deleted' | 'srcdeleted';
+  modTypeId: string;
+}
+
+function transform(source: string, files: IFileEntry[]): ISourceEntry {
+  return {
+    id: source,
+    filePaths: files.map(file => file.filePath),
+    action: files[0].action,
+    modTypeId: files[0].modTypeId,
+    type: files[0].type,
+  };
+}
+
+interface IComponentState {
+  showFiles: boolean;
+}
+
+class ExternalChangeDialog extends ComponentEx<IProps, IComponentState> {
   private setAll = {
     refchange: (evt: React.MouseEvent<any>) => {
       this.setAllFunc('refchange', evt.currentTarget.href.split('#')[1]);
@@ -73,37 +107,29 @@ class ExternalChangeDialog extends ComponentEx<IProps, { counter: number }> {
 
   constructor() {
     super();
-    this.state = { counter: 0 };
+    this.state = { showFiles: false };
   }
 
   public render(): JSX.Element {
     const { t, changes } = this.props;
-
-    const visible = changes !== undefined && changes.length > 0;
+    const { showFiles } = this.state;
 
     const refChanged = changes.filter(change => change.type === 'refchange');
     const valChanged = changes.filter(change => change.type === 'valchange');
     const deleted = changes.filter(change => change.type === 'deleted');
-    const srcdeleted = changes.filter(change => change.type === 'srcdeleted');
+    const srcDeleted = changes.filter(change => change.type === 'srcdeleted');
 
+    const renderFunc = showFiles ? this.renderFiles : this.renderSources;
     return (
-      <Modal id='ext-change-dialog' show={visible} onHide={nop}>
+      <Modal id='ext-change-dialog' show={changes.length > 0} onHide={nop}>
         <Modal.Header>
           <Modal.Title>{t('External Changes')}</Modal.Title>
         </Modal.Header>
-        <Modal.Body style={{ display: 'flex', flexDirection: 'column' }}>
-          <div className='padded-text' style={{ flex: 0 }} >
-            {t('One or more files have been modified on disk since Vortex last deployed. '
-              + 'You have to decide what happens with them.')}
-          </div>
-
-          {this.renderChanged(t('These files were modified'), 'valchange', valChanged)}
-          {this.renderChanged(t('These files were replaced (or removed from the mod)'),
-            'refchange', refChanged)}
-          {this.renderChanged(t('These files were deleted in output directory'),
-            'deleted', deleted)}
-          {this.renderChanged(t('These files were deleted in the mod directory'),
-            'srcdeleted', srcdeleted)}
+        <Modal.Body>
+          <Toggle checked={showFiles} onToggle={this.toggleShowFiles}>
+            {t('Show individual files')}
+          </Toggle>
+          {renderFunc(refChanged, valChanged, deleted, srcDeleted)}
         </Modal.Body>
         <Modal.Footer>
           <Button
@@ -125,7 +151,92 @@ class ExternalChangeDialog extends ComponentEx<IProps, { counter: number }> {
     );
   }
 
-  private renderChanged = (text: string, type: string, entries: IFileEntry[]) => {
+  private renderSources = (refChanged: IFileEntry[], valChanged: IFileEntry[],
+                           deleted: IFileEntry[], srcDeleted: IFileEntry[]): JSX.Element => {
+    const { t } = this.props;
+
+    const rcmap = refChanged.reduce(bySource, {});
+    const vcmap = valChanged.reduce(bySource, {});
+    const dmap = deleted.reduce(bySource, {});
+    const sdmap = srcDeleted.reduce(bySource, {});
+
+    const rc = Object.keys(rcmap).map(source => transform(source, rcmap[source]));
+    const vc = Object.keys(vcmap).map(source => transform(source, vcmap[source]));
+    const d = Object.keys(dmap).map(source => transform(source, dmap[source]));
+    const sd = Object.keys(sdmap).map(source => transform(source, sdmap[source]));
+
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <div className='padded-text' style={{ flex: 0 }} >
+          {t('One or more files have been modified on disk since Vortex last deployed. '
+            + 'You have to decide what happens with them.')}
+        </div>
+
+        {/* this.renderChangedSources(t('These mods were modified'), 'valchange', vc) */}
+        {this.renderChangedSources(t('Files were replaced'), 'refchange', rc)}
+        {this.renderChangedSources(t('Links were deleted'), 'deleted', d)}
+        {this.renderChangedSources(t('Source files were deleted'), 'srcdeleted', sd)}
+      </div>
+    );
+  }
+
+  private renderFiles = (refChanged: IFileEntry[], valChanged: IFileEntry[],
+                         deleted: IFileEntry[], srcDeleted: IFileEntry[]): JSX.Element => {
+    const { t, changes } = this.props;
+
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <div className='padded-text' style={{ flex: 0 }} >
+          {t('One or more files have been modified on disk since Vortex last deployed. '
+            + 'You have to decide what happens with them.')}
+        </div>
+
+        {/* this.renderChangedFile(t('These files were modified'), 'valchange', valChanged) */}
+        {this.renderChangedFile(t('Files were replaced'), 'refchange', refChanged)}
+        {this.renderChangedFile(t('Links were deleted'), 'deleted', deleted)}
+        {this.renderChangedFile(t('Source files were deleted'), 'srcdeleted', srcDeleted)}
+      </div>
+    );
+  }
+
+  private renderChangedSources = (text: string, type: string, entries: ISourceEntry[]) => {
+    const { t } = this.props;
+    if (entries.length === 0) {
+      return null;
+    }
+
+    const columns = this.genSourceColumns(type);
+
+    const actions = possibleActions[type];
+
+    return (
+      <div style={{ flex: '1 1 0', display: 'flex', flexDirection: 'column' }}>
+        {text}
+        <p>{actions.map(action => (
+          <a
+            key={action.key}
+            onClick={this.setAll[type]}
+            href={'#' + action.key}
+            style={{ marginRight: 10 }}
+          >{t(action.text)}
+          </a>
+        ))
+        }</p>
+        <div style={{ overflowY: 'auto', flex: '1 1 0' }}>
+          <Table
+            tableId={`external-change-${type}`}
+            data={entries}
+            actions={[]}
+            staticElements={columns}
+            showHeader={false}
+            showDetails={false}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  private renderChangedFile = (text: string, type: string, entries: IFileEntry[]) => {
     const { t } = this.props;
     if (entries.length === 0) {
       return null;
@@ -162,6 +273,50 @@ class ExternalChangeDialog extends ComponentEx<IProps, { counter: number }> {
     );
   }
 
+  private genSourceColumns(type: string): ITableAttribute[] {
+    const { onChangeAction } = this.props;
+
+    return [
+      {
+        id: 'name',
+        name: 'Mod Name',
+        description: 'Mod Name',
+        calc: (source: ISourceEntry) => source.id,
+        placement: 'table',
+        edit: {},
+      }, {
+        id: 'file_count',
+        name: 'File Count',
+        description: 'Number of files in this mod that were changed',
+        calc: (source: ISourceEntry, t: I18next.TranslationFunction) =>
+          t('{{count}} files', { count: source.filePaths.length }),
+        placement: 'table',
+        edit: {},
+      }, {
+        id: 'action',
+        name: 'Action',
+        description: 'the action to take on files in this mod',
+        calc: (source: ISourceEntry) =>
+          possibleActions[type].find(act => act.key === source.action).text,
+        placement: 'table',
+        edit: {
+          inline: true,
+          choices: () => possibleActions[type],
+          onChangeValue: (source: ISourceEntry, value: any) => {
+            let newAction = value;
+            if (value === undefined) {
+              const typeActions = possibleActions[type];
+              const idx = typeActions.findIndex(act => act.key === source.action);
+
+              newAction = typeActions[(idx + 1) % typeActions.length].key as FileAction;
+            }
+            source.filePaths.forEach(filePath => onChangeAction([filePath], newAction));
+          },
+        },
+      },
+    ];
+  }
+
   private genColumns(type: string): ITableAttribute[] {
     const { onChangeAction } = this.props;
 
@@ -196,6 +351,10 @@ class ExternalChangeDialog extends ComponentEx<IProps, { counter: number }> {
         },
       },
     ];
+  }
+
+  private toggleShowFiles = () => {
+    this.setState(update(this.state, { showFiles: { $set: !this.state.showFiles } }));
   }
 
   private setAllFunc = (changeType: string, action: FileAction) => {
