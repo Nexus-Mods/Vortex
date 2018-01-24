1d1cb1c4817cfb6447b8932c5fd4c8c9dd3aca8e
TanninOne <sherb@gmx.net>
2018-01-24 18:10:04 +0100

    version bump

--- src/extensions/category_management/reducers/category.ts
+++ src/extensions/category_management/reducers/category.ts
@@ -8,20 +8,16 @@ import * as actions from '../actions/category';
  */
 export const categoryReducer: IReducerSpec = {
   reducers: {
-    [actions.loadCategories as any]:
-        (state, payload) =>
-            setOrNop(state, [payload.gameId], payload.gameCategories),
-    [actions.setCategory as any]:
-        (state, payload) =>
-            setSafe(state, [payload.gameId, payload.id], payload.category),
-    [actions.removeCategory as any]:
-        (state, payload) => deleteOrNop(state, [payload.gameId, payload.id]),
-    [actions.updateCategories as any]:
-        (state, payload) =>
-            setSafe(state, [payload.gameId], payload.gameCategories),
-    [actions.renameCategory as any]:
-        (state, payload) => setOrNop(
-            state, [payload.gameId, payload.categoryId, 'name'], payload.name),
+    [actions.loadCategories as any]: (state, payload) =>
+      setOrNop(state, [payload.gameId], payload.gameCategories),
+    [actions.setCategory as any]: (state, payload) =>
+      setSafe(state, [payload.gameId, payload.id], payload.category),
+    [actions.removeCategory as any]: (state, payload) =>
+      deleteOrNop(state, [payload.gameId, payload.id]),
+    [actions.updateCategories as any]: (state, payload) =>
+      setSafe(state, [payload.gameId], payload.gameCategories),
+    [actions.renameCategory as any]: (state, payload) =>
+      setOrNop(state, [payload.gameId, payload.categoryId, 'name'], payload.name),
     [actions.setCategoryOrder as any]: (state, payload) => {
       const { gameId, categoryIds }: { gameId: string, categoryIds: string[] } = payload;
       let newState = state;
