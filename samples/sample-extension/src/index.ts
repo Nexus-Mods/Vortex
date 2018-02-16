c76bd4060af15e5596b8a4106a88a659025ee738
TanninOne <sherb@gmx.net>
2018-02-16 14:32:24 +0100

    updated sample extension

--- samples/sample-extension/src/index.ts
+++ samples/sample-extension/src/index.ts
@@ -1,8 +1,11 @@
+import { remote } from 'electron';
 import { log, types } from 'vortex-api';
 
 function main(context: types.IExtensionContext) {
-  context.registerAction('application-icons', 100, 'hand-peace-o', {}, 'Sample', () => {
-    log('info', 'hello world');
+  context.registerAction('global-icons', 100, 'menu', {}, 'Sample', () => {
+    remote.dialog.showMessageBox(remote.getCurrentWindow(), {
+      message: 'Hello World',
+    });
   });
   return true;
 }
