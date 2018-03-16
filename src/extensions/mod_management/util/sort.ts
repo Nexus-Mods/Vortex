f63e2acf1634750eee7cc68a1df7b055cdde0ab3
TanninOne <sherb@gmx.net>
2018-03-16 18:51:31 +0100

    graphical view for mod cycles (wip)

--- src/extensions/mod_management/util/sort.ts
+++ src/extensions/mod_management/util/sort.ts
@@ -1,15 +1,15 @@
+import { showDialog } from '../../../actions';
 import { IExtensionApi } from '../../../types/IExtensionContext';
 import { log } from '../../../util/log';
 import { getSafe } from '../../../util/storeHelper';
-import { truthy } from '../../../util/util';
 
 import { IMod } from '../types/IMod';
 
+import testModReference from './testModReference';
+
 import * as Promise from 'bluebird';
 import { alg, Graph } from 'graphlib';
-import * as minimatch from 'minimatch';
 import { ILookupResult, IReference, IRule, RuleType } from 'modmeta-db';
-import * as semver from 'semvish';
 
 interface IBiRule {
   subject: string;
@@ -17,39 +17,24 @@ interface IBiRule {
   type: RuleType;
 }
 
-function testRef(mod: IMod, ref: IReference): boolean {
-  const attr: any = mod.attributes;
-  // if reference is by file hash, use only that
-  if (ref.fileMD5 !== undefined) {
-    return attr.fileMD5 === ref.fileMD5;
-  }
-
-  // right file?
-  if (((ref.logicalFileName !== undefined) &&
-       (ref.logicalFileName !== attr.logicalFileName)) ||
-      ((ref.fileExpression !== undefined) &&
-       !minimatch(mod.installationPath, ref.fileExpression))) {
-    return false;
-  }
-
-  // right version? If no version is set we assume it's the right one,
-  // otherwise a no-version mod could never match
-  try {
-    if ((ref.versionMatch !== undefined) && truthy(attr.version) &&
-        !semver.satisfies(attr.version, ref.versionMatch)) {
-      return false;
-    }
-  } catch (err) {
-    // nop. This exception is definitively thrown by satisfies and this
-    // happens if the mod has no version specified or the versionMatch is
-    // invalid. Either way, it's safer to assume the rule does apply than not
-  }
-
-  return true;
+function findByRef(mods: IMod[], reference: IReference): IMod {
+  return mods.find((mod: IMod) => testModReference(mod, reference));
 }
 
-function findByRef(mods: IMod[], reference: IReference): IMod {
-  return mods.find((mod: IMod) => testRef(mod, reference));
+function showCycles(api: IExtensionApi, cycles: string[][]) {
+  api.store.dispatch(showDialog('error', 'Cycles', {
+    text: 'Dependency rules between your mods contain cycles, '
+      + 'like "A after B" and "B after A". You need to remove one of the '
+      + 'rules causing the cycle, otherwise your mods can\'t be '
+      + 'applied in the right order.',
+    links: cycles.map((cycle, idx) => (
+      { label: cycle.join(', '), action: () => {
+        api.events.emit('edit-mod-cycle', cycle);
+      } }
+    )),
+  }, [
+    { label: 'Close' },
+  ]));
 }
 
 function sortMods(gameId: string, mods: IMod[], api: IExtensionApi): Promise<string[]> {
@@ -82,31 +67,32 @@ function sortMods(gameId: string, mods: IMod[], api: IExtensionApi): Promise<str
   mods.forEach(mod => { dependencies.setNode(mod.id); });
 
   return Promise.map(mods, modMapper)
-      .catch((err: Error) => {
-        log('error', 'failed to sort mods',
-            {msg: err.message, stack: err.stack});
-      })
-      .then(() => {
-        try {
-          return Promise.resolve(alg.topsort(dependencies));
-        } catch (err) {
-          // exception type not included in typings
-          if (err instanceof (alg.topsort as any).CycleException) {
-            api.showErrorNotification('Mod rules contain cycles',
-              'Dependency rules between your mods contain cycles, '
-                    + 'like "A after B" and "B after A". You need to remove one of the '
-                    + 'rules causing the cycle, otherwise your mods can\'t be '
-                    + 'applied in the right order.<br/><ul>'
-                    + renderCycles(alg.findCycles(dependencies))
-                    + '</ul>'
-              , { id: 'mod-cycles', isHTML: true, allowReport: false });
-            // return unsorted
-            return Promise.resolve(mods.map(mod => mod.id));
-          } else {
-            return Promise.reject(err);
-          }
+    .catch((err: Error) => {
+      log('error', 'failed to sort mods',
+          {msg: err.message, stack: err.stack});
+    })
+    .then(() => {
+      try {
+        return Promise.resolve(alg.topsort(dependencies));
+      } catch (err) {
+        // exception type not included in typings
+        if (err instanceof (alg.topsort as any).CycleException) {
+          api.sendNotification({
+            type: 'warning',
+            message: 'Mod rules contain cycles',
+            actions: [
+              { title: 'Show', action: () => {
+                showCycles(api, alg.findCycles(dependencies));
+              } },
+            ],
+          });
+          // return unsorted
+          return Promise.resolve(mods.map(mod => mod.id));
+        } else {
+          return Promise.reject(err);
         }
-      });
+      }
+    });
 }
 
 function renderCycles(cycles: string[][]): string {
