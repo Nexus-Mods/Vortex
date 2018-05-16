41289bf895eab8ee590288d232ccb1ddaf589a6b
TanninOne <sherb@gmx.net>
2018-05-16 11:28:44 +0200

    fixed source map support in development

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -19,6 +19,13 @@ module.exports = {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         exclude: /node_modules/,
+        options: {
+          compilerOptions: {
+            sourceMap: true,
+            inlineSourceMap: false,
+            inlineSources: false,
+          }
+        }
       },
     ]
   },
