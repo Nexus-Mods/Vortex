2446e30b0df8fc4dd5789ec7d2d64cf68308f3a0
TanninOne <sherb@gmx.net>
2018-06-05 15:57:48 +0200

    cleanup

--- src/extensions/nexus_integration/texts.ts
+++ src/extensions/nexus_integration/texts.ts
@@ -7,22 +7,6 @@ import * as path from 'path';
 
 function getText(id: string, t: I18next.TranslationFunction): Promise<string> {
   switch (id) {
-    case 'apikey':
-      return t(
-        'Instead of entering your username and password for login you need to enter '
-        + 'an API-Key that you can generate on the nexusmods page website. Such keys '
-        + 'can also be used for other applications using nexus features. The key '
-        + 'identifies you and the application it\'s intended for.\n\n'
-        + 'Apart from this being more secure, it gives you and us more control:\n'
-        + '- you only ever type your password on nexusmods\n'
-        + '- If someone steals your api key they can\'t log in to the page and can\'t change '
-        + 'your email or password\n'
-        + '- You can delete/disable an api key without affecting your login or other api keys\n'
-        + '- You can change your password without having to update it anywhere outside the page.\n'
-        + '- If an application is malicious or spams the page with requests, affecting '
-        + 'other users, we can identify the application as the offender, instead of seeing '
-        + 'just the ips/accounts responsible for the traffic.\n\n'
-        + 'For this reason, please always use keys in the applications they were intended for.');
     case 'chrome-fix': return chromePath().then(resolvedPath => t(
       'Chrome requires a change to a configuration file to allow an external program to handle '
       + 'links where chrome doesn\'t know the protocol. This is the case for the green "Download '
