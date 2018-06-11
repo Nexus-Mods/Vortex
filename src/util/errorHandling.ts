1bd7e8a2505826ce39ffdfae9f2b80afd828c2cc
TanninOne <sherb@gmx.net>
2018-06-11 14:11:55 +0200

    fixed: unhandled exception during error reporting

--- src/util/errorHandling.ts
+++ src/util/errorHandling.ts
@@ -163,8 +163,8 @@ function nexusReport(hash: string, type: string, error: IError, labels: string[]
     hash,
     referenceId))
   .then(() => opn(`https://www.nexusmods.com/crash-report/?key=${referenceId}`)
-      .catch(err => undefined))
-  .then(() => undefined);
+  .then(() => undefined)
+  .catch(err => undefined));
 }
 
 let fallbackAPIKey: string;
