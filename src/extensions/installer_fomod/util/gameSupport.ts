5307d3e7ef3806b729e38e533d579b0521087e78
TanninOne <sherb@gmx.net>
2018-07-23 14:36:58 +0200

    "calientetools" added to list of stop-folders for gamebryo games

--- src/extensions/installer_fomod/util/gameSupport.ts
+++ src/extensions/installer_fomod/util/gameSupport.ts
@@ -13,7 +13,7 @@ function toWordExp(input: string): string {
 
 const gamebryoTopLevel: string[] = ['distantlod', 'textures', 'meshes', 'music', 'shaders', 'video',
       'interface', 'fonts', 'scripts', 'facegen', 'menus', 'lodsettings', 'lsdata', 'sound',
-      'strings', 'trees', 'asi', 'tools'];
+      'strings', 'trees', 'asi', 'tools', 'calientetools'];
 
 const gamebryoPatterns: string[] = [
   '[^/]*\\.esp$',
