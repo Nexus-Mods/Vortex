Nexus-Mods/Vortex

commit 601542c0637089e9d5052dd9765b52f666cf0090

Allow filtering by parent categories even when they themselve don't contain anything
fixes #811

src/extensions/category_management/util/CategoryFilter.tsx
@@ -59,9 +59,10 @@ class CategoryFilterComponent extends React.Component<IProps, IComponentState> {
     const modCategories = new Set<string>();
     Object.keys(mods || {}).forEach(modId => {
       const mod = mods[modId];
-      const category = getSafe(mod.attributes, ['category'], undefined);
-      if (category !== undefined) {
+      let category = getSafe(mod.attributes, ['category'], undefined);
+      while (category !== undefined) {
         modCategories.add(category.toString());
+        category = categories[category].parentCategory;
       }
       if (mod.archiveId !== undefined) {
         installedArchives.add(mod.archiveId);

commit d3beefb0a61dfb10b2f22970806c89665642b69b

fixed: exception if mod refers to unknown category (not released)
introduced in 601542c0

src/extensions/category_management/util/CategoryFilter.tsx
@@ -61,8 +61,12 @@ class CategoryFilterComponent extends React.Component<IProps, IComponentState> {
       const mod = mods[modId];
       let category = getSafe(mod.attributes, ['category'], undefined);
       while (category !== undefined) {
-        modCategories.add(category.toString());
-        category = categories[category].parentCategory;
+        if (categories[category] !== undefined) {
+          modCategories.add(category.toString());
+          category = categories[category].parentCategory;
+        } else {
+          category = undefined;
+        }
       }
       if (mod.archiveId !== undefined) {
         installedArchives.add(mod.archiveId);