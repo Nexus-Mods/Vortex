de7c4da0e95d4e95da25e1fca5de7305a8eb2ff9
TanninOne <seppleviathan@gmx.de>
2018-10-29 12:59:38 +0100

    http status codes case insensitive

--- src/extensions/download_management/views/DownloadView.tsx
+++ src/extensions/download_management/views/DownloadView.tsx
@@ -296,7 +296,7 @@ class DownloadView extends ComponentEx<IDownloadViewProps, IComponentState> {
                                    'Sorry, this download is missing info necessary to resume. '
                                    + 'Please try restarting it.',
                                    undefined, false);
-          } else if (['Moved Permanently', 'Forbidden', 'Gone'].indexOf(err.HTTPStatus) !== -1) {
+          } else if (['moved permanently', 'forbidden', 'gone'].indexOf(err.HTTPStatus.toLowerCase()) !== -1) {
             this.props.onShowError('Failed to resume download', 'Sorry, the download link is no longer valid. '
                                  + 'Please restart the download.',
               undefined, false);
