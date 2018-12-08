3c6a09a4b65e092ccfc1432111ab49e11f8bc696
TanninOne <seppleviathan@gmx.de>
2018-12-08 16:35:51 +0100

    removed usvfs deployment
    moved to its own repository

--- extensions/usvfs-deploy/webpack.config.js
+++
@@ -1,6 +0,0 @@
-const webpack = require('vortex-api/bin/webpack').default;
-
-const config = webpack('usvfs-deployment', __dirname, 4);
-config.externals.iconv = 'iconv-lite';
-
-module.exports = config;
