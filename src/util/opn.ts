f70d2f8843837ba0ff9237571e287812c6e82c1f
TanninOne <seppleviathan@gmx.de>
2019-01-28 17:18:46 +0000

    report missing application association inside vortex
    instead of letting windows produce a native error dialog

--- src/util/opn.ts
+++ src/util/opn.ts
@@ -1,15 +1,20 @@
 import * as Promise from 'bluebird';
-import opn = require('opn');
 import * as winapi from 'winapi-bindings';
+import { MissingInterpreter } from './api';
+import opn = require('opn');
 
 function open(target: string, wait?: boolean): Promise<void> {
   if ((winapi !== undefined) && !wait) {
     try {
       // TODO: technically with ShellExecuteEx we should be able to reproduce the wait behaviour
-      winapi.ShellExecuteEx({ verb: 'open', show: 'restore', file: target });
+      winapi.ShellExecuteEx({ verb: 'open', show: 'restore', file: target, mask: ['flag_no_ui'] });
       return Promise.resolve();
     } catch (err) {
-      return Promise.reject(err);
+      if (err.errno === 1155) {
+        return Promise.reject(new MissingInterpreter('No default application set up for file type.', err.path));
+      } else {
+        return Promise.reject(err);
+      }
     }
   } else {
     return Promise.resolve(opn(target, {
