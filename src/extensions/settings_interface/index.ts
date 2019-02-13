61bf6372224df946160db80d78f0a49782d1c13e
adrian <idc.dts@gmail.com>
2019-02-13 07:18:34 +0000

    Fixed: update main page immediately on advanced/profilesVisible state change

--- src/extensions/settings_interface/index.ts
+++ src/extensions/settings_interface/index.ts
@@ -8,6 +8,16 @@ function init(context: IExtensionContext): boolean {
   context.registerReducer(['settings', 'interface'], settingsReducer);
   context.registerReducer(['settings', 'automation'], automationReducer);
 
+  context.once(() => {
+    context.api.onStateChange(['settings', 'interface', 'advanced'], () => {
+      context.api.events.emit('refresh-main-page');
+    });
+
+    context.api.onStateChange(['settings', 'interface', 'profilesVisible'], () => {
+      context.api.events.emit('refresh-main-page');
+    });
+  })
+
   return true;
 }
 
