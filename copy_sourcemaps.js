e21cb48f5ce20b6d9bda6b9719e28b2ea296da23
TanninOne <seppleviathan@gmx.de>
2019-03-07 12:15:42 +0100

    added script to create sourcemaps directory

---
+++ copy_sourcemaps.js
@@ -0,0 +1,9 @@
+const copyfiles = require('copyfiles');
+const fs = require('fs');
+const path = require('path');
+
+const pack = JSON.parse(fs.readFileSync(path.join('app', 'package.json')));
+
+copyfiles(['app/**/*.js.map', 'app/**/*.js', `sourcemaps/${pack.version}`], {up: 1}, () => {
+  console.log('done');
+});
