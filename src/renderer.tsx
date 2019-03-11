48465269a2c4a6e266f2f31a8e022caf9f315013
TanninOne <seppleviathan@gmx.de>
2019-03-11 14:11:38 +0100

    suppress exception from Packery
    the (ultra rare) exception happens in some callback internal to packery so we have no way of catching it and it doesn't look like Packery is very actively maintained (one single commit in almost a year) so this is probably the best we can do
    
    fixes #308

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -152,6 +152,7 @@ function errorHandler(evt: any) {
           (error.message === 'socket hang up')
           || (error.stack.indexOf('net::ERR_CONNECTION_RESET') !== -1)
           || (error.stack.indexOf('net::ERR_ABORTED') !== -1)
+          || (error.stack.indexOf('PackeryItem.proto.positionDropPlaceholder') !== -1)
          )
       ) {
     log('warn', 'suppressing error message', { message: error.message, stack: error.stack });
