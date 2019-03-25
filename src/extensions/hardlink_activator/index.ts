600f622224a7cc53d326fc95a08be935db43b01d
TanninOne <seppleviathan@gmx.de>
2019-03-25 16:01:11 +0100

    fixed: obscure crash on hardlink deployment
    seems to have happened only once?
    
    fixes #3684

--- src/extensions/hardlink_activator/index.ts
+++ src/extensions/hardlink_activator/index.ts
@@ -170,6 +170,7 @@ class DeploymentMethod extends LinkingDeployment {
 
   public postPurge(): Promise<void> {
     delete this.mInstallationFiles;
+    this.mInstallationFiles = undefined;
     return Promise.resolve();
   }
 
@@ -183,6 +184,11 @@ class DeploymentMethod extends LinkingDeployment {
       this.mInstallationFiles = new Set<number>();
       installEntryProm = turbowalk(installationPath,
         entries => {
+          if (this.mInstallationFiles === undefined) {
+            // don't know when this would be necessary but apparently
+            // it is, see https://github.com/Nexus-Mods/Vortex/issues/3684
+            return;
+          }
           entries.forEach(entry => {
             if (entry.linkCount > 1) {
               this.mInstallationFiles.add(entry.id);
@@ -257,7 +263,13 @@ class DeploymentMethod extends LinkingDeployment {
 
   private ensureDir(dirPath: string): Promise<void> {
     return (this.mDirCache === undefined) || !this.mDirCache.has(dirPath)
-      ? fs.ensureDirAsync(dirPath).then(created => { this.mDirCache.add(dirPath); return created; })
+      ? fs.ensureDirAsync(dirPath).then(created => {
+        if (this.mDirCache === undefined) {
+          this.mDirCache = new Set<string>();
+        }
+        this.mDirCache.add(dirPath);
+        return created;
+      })
       : Promise.resolve(null);
   }
 }
