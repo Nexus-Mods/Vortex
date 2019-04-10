5b9eef77d494894d3ea04d0405c45a205abb32b9
TanninOne <seppleviathan@gmx.de>
2019-04-10 13:36:17 +0200

    added tooltip bbcode tag

---
+++ src/util/bbcode/TooltipTag.tsx
@@ -0,0 +1,19 @@
+import { Tag } from 'bbcode-to-react';
+import * as React from 'react';
+
+function nop() {
+
+}
+
+class TooltipTag extends Tag {
+  public toHTML(): string[] {
+    return [this.getContent()];
+  }
+
+  public toReact() {
+    const { tooltip } = this.params;
+    return <a onClick={nop} title={tooltip}>{this.getComponents()}</a>;
+  }
+}
+
+export default TooltipTag;
