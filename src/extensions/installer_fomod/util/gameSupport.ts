3ef738540d8da2eb85e3fe42825d75fc42226544
IDCs <idc.dts@gmail.com>
2019-07-24 11:04:02 +0100

    Added to Sekiro stop patterns

--- src/extensions/installer_fomod/util/gameSupport.ts
+++ src/extensions/installer_fomod/util/gameSupport.ts
@@ -56,7 +56,9 @@ function stopPatterns(gameMode: string) {
               'models', 'particles', 'python', 'resource', 'save', 'scripts', 'sound',
               'vdata'].map(toWordExp));
     case 'sekiro':
-      return ['menu', 'sound', 'parts'].map(toWordExp);
+      return ['action', 'Artwork_MiniSoundtrack', 'chr', 'cutscene', 'event', 'facegen', 'font',
+              'map', 'menu', 'movie', 'msg', 'mtd', 'obj', 'other', 'param', 'parts', 'script',
+              'sfx', 'shader', 'sound'].map(toWordExp);
     case 'darkestdungeon':
       return ['audio', 'campaign', 'colours', 'curios', 'cursors', 'dlc', 'dungeons',
               'effects', 'fe_flow', 'fonts', 'fx', 'heroes', 'inventory', 'loading_screen',
