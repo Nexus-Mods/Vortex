79f75655d2c3dbf7f26772c6bbec366ed2b8c4ce
TanninOne <seppleviathan@gmx.de>
2019-12-02 15:06:28 +0000

    don't install update and warn user if registry key missing
    It's assumed that this key tells the installer where a custom install has been installed to and  the update wouldn't install to that location if the registry key is missing

--- src/extensions/updater/autoupdater.ts
+++ src/extensions/updater/autoupdater.ts
@@ -3,19 +3,43 @@ import { IExtensionApi } from '../../types/IExtensionContext';
 import { IState } from '../../types/IState';
 import { UserCanceled } from '../../util/api';
 import { log } from '../../util/log';
+import opn from '../../util/opn';
 import { truthy } from '../../util/util';
 
 import {app as appIn, ipcMain, remote} from 'electron';
 import {autoUpdater as AUType, UpdateInfo} from 'electron-updater';
+import { RegGetValue } from 'winapi-bindings';
 import * as semver from 'semver';
+import * as uuidv5 from 'uuid/v5';
 
 const app = remote !== undefined ? remote.app : appIn;
+
+const appName = 'com.nexusmods.vortex';
+const ELECTRON_BUILDER_NS_UUID = '50e065bc-3134-11e6-9bab-38c9862bdaf3'
+
+const myguid = (() => {
+  let cached: string;
+  return () => {
+    if (cached === undefined) {
+      cached = uuidv5(appName, ELECTRON_BUILDER_NS_UUID);
+    }
+    return cached;
+  };
+})();
 
 interface IProgressInfo {
   bps: number;
   percent: number;
   total: number;
   transferred: number;
+}
+
+function openStable() {
+  opn('https://www.nexusmods.com/site/mods/1').catch(() => null);
+}
+
+function openTesting() {
+  opn('https://www.github.com/Nexus-Mods/Vortex#release').catch(() => null);
 }
 
 function setupAutoUpdate(api: IExtensionApi) {
@@ -85,10 +109,43 @@ function setupAutoUpdate(api: IExtensionApi) {
   });
 
   autoUpdater.on('update-available', (info: UpdateInfo) => {
+    let instPath: string;
+    if (process.platform === 'win32') {
+      try {
+        instPath = RegGetValue('HKEY_LOCAL_MACHINE', `SOFTWARE\\${myguid()}`, 'InstallLocation').value as string;
+      } catch (err) {
+        api.sendNotification({
+          type: 'warning',
+          message: 'Update can\'t be installed automatically',
+          actions: [
+            { title: 'More', action: dismiss => {
+              api.showDialog('info', 'Update can\'t be installed automatically', {
+                text: 'An update for Vortex is available but it can\'t be installed automatically because '
+                  + 'a necessary registry key has been removed. Please install the latest version of Vortex manually.',
+              }, [
+                { label: 'Close' },
+                { label: 'Open Page', action: () => {
+                  const { channel } = state.settings.update;
+                  if (channel === 'beta') {
+                    openTesting();
+                  } else {
+                    openStable();
+                  }
+                  dismiss();
+                } },
+              ]);
+            } },
+          ]
+        });
+        return;
+      }
+    }
     log('info', 'update available', {
       current: app.getVersion(),
       update: info.version,
+      instPath,
     });
+    
     queryUpdate(info.version)
       .then(() => autoUpdater.downloadUpdate()
         .catch(err => {
