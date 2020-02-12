60278f4a4ff3299a581ce25a8b1cef5715dcef5d
TanninOne <seppleviathan@gmx.de>
2020-02-12 10:41:24 +0100

    fixed: elevation didn't work

--- src/util/vortex-run/lib/elevated.js
+++ src/util/vortex-run/lib/elevated.js
@@ -1,10 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const bluebird_1 = require("bluebird");
-const fs = require("fs");
-const path = require("path");
-const tmp = require("tmp");
-const winapi = require("winapi-bindings");
+const bluebird_1 = __importDefault(require("bluebird"));
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const tmp = __importStar(require("tmp"));
+const winapi = __importStar(require("winapi-bindings"));
 function elevatedMain(moduleRoot, ipcPath, main) {
     const handleError = (error) => {
         // tslint:disable-next-line:no-console
--- src/util/vortex-run/lib/index.js
+++ src/util/vortex-run/lib/index.js
@@ -1,8 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const elevated_1 = require("./elevated");
+const elevated_1 = __importDefault(require("./elevated"));
 exports.runElevated = elevated_1.default;
-const thread_1 = require("./thread");
+const thread_1 = __importDefault(require("./thread"));
 exports.runThreaded = thread_1.default;
 const dynreq = require;
 exports.dynreq = dynreq;
