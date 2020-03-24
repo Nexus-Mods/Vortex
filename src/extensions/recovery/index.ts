29828d10b11c0ebd52aac3fe71894a804b0baab4
TanninOne <seppleviathan@gmx.de>
2020-03-24 11:18:59 +0100

    added option to reset enabled mods to manifest
    this can help in case of a catastrophic breakage in the application state but also might be useful as an "undo" function

--- src/extensions/recovery/index.ts
+++ src/extensions/recovery/index.ts
@@ -1,9 +1,18 @@
 // features to help restore vortex to a working state
 
+import { IDialogResult } from '../../types/IDialog';
 import { IExtensionApi, IExtensionContext } from '../../types/IExtensionContext';
+import { activeProfile, currentGameDiscovery } from '../../util/selectors';
+
+import { getGame } from '../gamemode_management/util/getGame';
 
 import { createFullStateBackup } from '../../util/store';
 
+import { setModEnabled } from '../../actions';
+import { IDeploymentManifest } from '../../types/api';
+import { UserCanceled } from '../../util/CustomErrors';
+import { getSafe } from '../../util/storeHelper';
+import { getManifest } from '../mod_management/util/activationStore';
 import Workarounds from './Workarounds';
 
 const ONE_HOUR = 60 * 60 * 1000;
@@ -32,7 +41,63 @@ function createBackup(api: IExtensionApi, name: string) {
         ],
       });
     });
+}
+
+async function resetToManifest(api: IExtensionApi) {
+  try {
+    const state = api.getState();
+    const profile = activeProfile(state);
+    const game = getGame(profile.gameId);
+    const discovery = currentGameDiscovery(state);
+
+    const modPaths = game.getModPaths(discovery.path);
+    const modTypes = Object.keys(modPaths);
+
+    const enabledMods = new Set<string>();
+
+    await Promise.all(modTypes.map(async modType => {
+      const manifest: IDeploymentManifest = await getManifest(api, modType, profile.gameId);
+      manifest.files.forEach(file => {
+        enabledMods.add(file.source);
+        file.merged.forEach(merged => enabledMods.add(merged));
+      });
+    }));
+
+    if (enabledMods.size === 0) {
+      api.sendNotification({
+        type: 'info',
+        message: 'No mods currently deployed',
+      });
+    }
+
+    const dialogResult: IDialogResult = await api.showDialog(
+      'question', 'Reset to last deployment', {
+      text: 'This will enable all mods that were enabled for '
+        + 'the last deployment and disable all that weren\'t. '
+        + 'It doesn\'t revert anything else like mod order rules.',
+    }, [
+      { label: 'Cancel' },
+      { label: 'Continue', default: true },
+    ]);
+
+    if (dialogResult.action === 'Cancel') {
+      return;
+    }
+
+    const mods = state.persistent.mods[profile.gameId];
+    const isEnabled = modId => getSafe(profile, ['modState', modId, 'enabled'], false);
+    Object.keys(mods)
+      .forEach(modId => {
+        if (isEnabled(modId) !== enabledMods.has(modId)) {
+          api.store.dispatch(setModEnabled(profile.id, modId, enabledMods.has(modId)));
+        }
+      });
+  } catch (err) {
+    if (!(err instanceof UserCanceled)) {
+      api.showErrorNotification('Failed to reset mod', err);
+    }
   }
+}
 
 function init(context: IExtensionContext): boolean {
   context.registerSettings('Workarounds', Workarounds, () => ({
@@ -44,6 +109,10 @@ function init(context: IExtensionContext): boolean {
         }));
     },
   }));
+
+  context.registerAction('mod-icons', 115, 'undo', {}, 'Reset to manifest', () => {
+    resetToManifest(context.api);
+  });
 
   context.onceMain(() => {
     setInterval(() => createBackup(context.api, 'hourly'), ONE_HOUR);
