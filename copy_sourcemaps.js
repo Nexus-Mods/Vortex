b251e37368a6e2ab3d3a64050514bbb293cd29fc
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2020-04-30 18:05:56 -0700

    Start of switching to single package.json
    Delete and ignore app directory and update a couple script paths

--- copy_sourcemaps.js
+++ copy_sourcemaps.js
@@ -1,9 +1,8 @@
 const copyfiles = require('copyfiles');
 const fs = require('fs');
-const path = require('path');
 
-const pack = JSON.parse(fs.readFileSync(path.join('app', 'package.json')));
+const pack = JSON.parse(fs.readFileSync('package.json'));
 
-copyfiles(['app/**/*.js.map', 'app/**/*.js', `sourcemaps/${pack.version}`], {up: 1}, () => {
+copyfiles(['app/**/*.js.map', 'app/**/*.js', `sourcemaps/${pack.version}`], { up: 1 }, () => {
   console.log('done');
 });
