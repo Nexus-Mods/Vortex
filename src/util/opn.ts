06dc329eb277e32208f896b2f83b138949008109
TanninOne <seppleviathan@gmx.de>
2020-05-13 16:18:37 +0200

    force applications started with opn to foreground

--- src/util/opn.ts
+++ src/util/opn.ts
@@ -4,11 +4,25 @@ import Promise from 'bluebird';
 import opn = require('opn');
 import * as winapi from 'winapi-bindings';
 
+import {ipcMain, ipcRenderer} from 'electron';
+
+// apparently the browser process is treated as the foreground process and only it
+// can bring a window to the foreground
+if (ipcMain !== undefined && (winapi?.ShellExecuteEx !== undefined)) {
+  ipcMain.on('__opn_win32', (evt, target) => {
+    winapi.ShellExecuteEx({ verb: 'open', show: 'foreground' as any, file: target, mask: ['flag_no_ui'] });
+  });
+}
+
 function open(target: string, wait?: boolean): Promise<void> {
+  // TODO: technically with ShellExecuteEx we should be able to reproduce the wait behaviour
   if ((winapi?.ShellExecuteEx !== undefined) && !wait) {
     try {
-      // TODO: technically with ShellExecuteEx we should be able to reproduce the wait behaviour
-      winapi.ShellExecuteEx({ verb: 'open', show: 'restore', file: target, mask: ['flag_no_ui'] });
+      if (ipcRenderer !== undefined) {
+        ipcRenderer.send('__opn_win32', target);
+      } else {
+        winapi.ShellExecuteEx({ verb: 'open', show: 'foreground' as any, file: target, mask: ['flag_no_ui'] });
+      }
       return Promise.resolve();
     } catch (err) {
       if (err.errno === 1155) {
