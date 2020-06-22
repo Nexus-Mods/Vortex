7e142e48207f58feeea82dbbc06c03460b552324
TanninOne <seppleviathan@gmx.de>
2020-06-22 09:27:41 +0200

    fixed: crash opening webpage for uninstalled mods
    fixes #6634

--- src/extensions/nexus_integration/index.tsx
+++ src/extensions/nexus_integration/index.tsx
@@ -817,7 +817,7 @@ function init(context: IExtensionContextExt): boolean {
                           undefined);
       if (ids !== undefined) {
         context.api.events.emit('open-mod-page',
-                                ids.gameId || gameMode, ids.modId, mod.attributes?.source);
+                                ids.gameId || gameMode, ids.modId, 'nexus');
       }
     }
   }, instanceIds => {
