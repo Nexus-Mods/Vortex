fe1cfb945e21cdd761f63659f241681e522d6f1d
TanninOne <seppleviathan@gmx.de>
2020-09-11 12:00:19 +0200

    fixed: exception running a tool if no profile active
    not sure how the user managed that though
    
    fixes #7387

--- src/extensions/tool_variables_base/index.ts
+++ src/extensions/tool_variables_base/index.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 import { IExtensionContext, IRunParameters } from '../../types/IExtensionContext';
 import { IProfile, IState } from '../../types/IState';
 import getVortexPath from '../../util/getVortexPath';
+import { log } from '../../util/log';
 import { activeProfile } from '../../util/selectors';
 
 function init(context: IExtensionContext): boolean {
@@ -11,14 +12,27 @@ function init(context: IExtensionContext): boolean {
     const state: IState = context.api.getState();
     const profile: IProfile = activeProfile(state);
 
-    return {
+    if (profile === undefined) {
+      log('warn', 'starting a tool with no active tool? How?', { parameters });
+      // not really the job of this extension to report this to the use
+    }
+
+    let res: any = {
       ...Object.keys(env).reduce((prev, key) => ({ ...prev, [key.toUpperCase()]: env[key] }), {}),
       APPDATA: getVortexPath('appData'),
       HOME: os.homedir(),
-      PROFILE_ID: profile.id,
-      PROFILE_NAME: profile.name,
-      PROFILE_PATH: path.join(getVortexPath('userData'), profile.gameId, 'profiles', profile.id),
     };
+
+    if (profile !== undefined) {
+      res = {
+        ...res,
+        PROFILE_ID: profile.id,
+        PROFILE_NAME: profile.name,
+        PROFILE_PATH: path.join(getVortexPath('userData'), profile.gameId, 'profiles', profile.id),
+      };
+    }
+
+    return res;
   });
 
   return true;
