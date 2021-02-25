9b969fef1b801ab6f0b0a12571ff905a7ddafffc
idcs <idc.dts@gmail.com>
2021-02-25 07:06:11 +0000

    minor "typo" fix
    pretty sure that "Symlinks not supported" and "Symlinks are not
    supported" are both correct but meh.
    
    fixes nexus-mods/vortex#8616

--- src/extensions/symlink_activator_elevate/index.ts
+++ src/extensions/symlink_activator_elevate/index.ts
@@ -141,7 +141,7 @@ class DeploymentMethod extends LinkingDeployment {
       lastReport = report;
 
       if (report === 'not-supported') {
-        api.showErrorNotification('Symlinks not support',
+        api.showErrorNotification('Symlinks are not support',
           'It appears symbolic links aren\'t supported between your mod staging folder and game '
           + 'folder. On Windows, symbolic links only work on NTFS drives.', { allowReport: false });
       } else {
