b4c7a575ac0dd8431b878ba182c2d5eb1b875141
idcs <idc.dts@gmail.com>
2021-05-03 16:46:09 +0100

    removing non-standard uppercase directory in api

--- src/types/Collections/IGameSpecificInterfaceProps.ts
+++
@@ -1,8 +0,0 @@
-import { IRevision } from '@nexusmods/nexus-api';
-import * as types from '../api';
-
-export interface IGameSpecificInterfaceProps {
-  t: types.TFunction;
-  collection: types.IMod;
-  revisionInfo: IRevision;
-}
