7d0e01628a61b156ed8e57d22de1182c921c3f96
idcs <idc.dts@gmail.com>
2021-09-17 11:50:16 +0100

    updated extensions

--- src/extensions/extension_manager/index.ts
+++ src/extensions/extension_manager/index.ts
@@ -299,7 +299,9 @@ function init(context: IExtensionContext) {
       const modId = state.persistent.downloads.files[archiveId]?.modInfo?.nexus?.ids?.modId;
       const ext = state.session.extensions.available.find(iter => iter.modId === modId);
       const isInstalled = Object.values(state.session.extensions.installed).find(inst =>
-        (inst.modId === ext?.modId) && (inst.version === ext?.version)) !== undefined;
+        (!!inst?.modId) // Corrupt state ? (#9935)
+          && (inst.modId === ext?.modId)
+          && (inst.version === ext?.version)) !== undefined;
       if (isInstalled) {
         context.api.sendNotification({
           id: 'extension-already-installed',
