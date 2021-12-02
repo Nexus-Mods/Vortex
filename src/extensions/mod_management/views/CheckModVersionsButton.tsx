6c10a59980fa86d019a61f3a9714e15a6ea013fa
idcs <idc.dts@gmail.com>
2021-12-02 08:03:35 +0000

    fixed minor typo in checking for updates notification
    closes nexus-mods/vortex#10141

--- src/extensions/mod_management/views/CheckModVersionsButton.tsx
+++ src/extensions/mod_management/views/CheckModVersionsButton.tsx
@@ -77,7 +77,7 @@ class CheckVersionsButton extends ComponentEx<IProps, {}> {
 
         this.context.api.sendNotification({
           type: 'success',
-          message: 'Check for mod updates complete ({{count}} update found)',
+          message: 'Check for mod updates complete ({{count}} updates found)',
           replace: {
             count: modIds.length,
           },
