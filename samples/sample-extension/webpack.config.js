1fe7d09ddbe54ea8e4624c0d1944f6b837a27835
TanninOne <seppleviathan@gmx.de>
2022-02-07 14:25:29 +0100

    updated sample to use webpack 5

--- samples/sample-extension/webpack.config.js
+++ samples/sample-extension/webpack.config.js
@@ -1,4 +1,4 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('sample-extension', __dirname, 4);
+module.exports = webpack('sample-extension', __dirname, 5);
 
