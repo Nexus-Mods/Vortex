b285ea5a83d1cd40e718677d194c1aa1d6370087
TanninOne <seppleviathan@gmx.de>
2022-03-16 16:14:47 +0100

    removed option to select "Next" update channel

--- src/extensions/updater/SettingsUpdate.tsx
+++ src/extensions/updater/SettingsUpdate.tsx
@@ -40,7 +40,6 @@ class SettingsUpdate extends ComponentEx<IProps, {}> {
           >
             <option value='stable'>{t('Stable')}</option>
             <option value='beta'>{t('Beta')}</option>
-            <option value='next'>{t('Next (Feature Preview)')}</option>
             <option value='none'>{t('No automatic updates')}</option>
           </FormControl>
           <ControlLabel>
