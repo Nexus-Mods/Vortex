70f0bc55ed2887708caf10b31901a54160722afa
TanninOne <seppleviathan@gmx.de>
2022-05-18 14:09:51 +0200

    dropped workaround for exe icons

--- src/util/exeIcon.ts
+++ src/util/exeIcon.ts
@@ -1,16 +1,22 @@
-import { app } from 'electron';
-import * as iconExtract from 'icon-extract';
+import makeRemoteCall from './electronRemote';
 import * as fs from './fs';
 
-const getFileIcon = (process.type === 'renderer')
-  // tslint:disable-next-line:no-var-requires
-  ? require('@electron/remote').app.getFileIcon
-  : app.getFileIcon;
+const efi = makeRemoteCall('extract-file-icon',
+    (electron, content, exePath: string, iconPath: string) => {
+  return electron.app.getFileIcon(exePath, { size: 'normal' })
+    .then(icon => fs.writeFileAsync(iconPath, icon.toPNG()))
+    .then(() => null);
+});
 
 function extractExeIcon(exePath: string, destPath: string): Promise<void> {
+  // app.getFileIcon generated broken output on windows as of electron 11.0.4
+  // (see https://github.com/electron/electron/issues/26918)
+  // This issue has not been closed or so much as been replied to, however I was not able to
+  // reproduce it so I'm tentatively removing the windows-specific workaround as of
+  // Vortex 1.6.0
+
+  /*
   if (process.platform === 'win32') {
-    // app.getFileIcon generates broken output as of electron 11.0.4, afaik this
-    // is limited to windows (see https://github.com/electron/electron/issues/26918)
     return new Promise((resolve, reject) => {
       iconExtract.extractIconToFile(exePath, destPath, error => {
         if (error !== null) {
@@ -21,9 +27,9 @@ function extractExeIcon(exePath: string, destPath: string): Promise<void> {
       });
     });
   } else {
-    getFileIcon(exePath, { size: 'normal' })
-      .then(icon => fs.writeFileAsync(destPath, icon.toPNG()));
-  }
+  */
+    return efi(exePath, destPath);
+  // }
 }
 
 export default extractExeIcon;
