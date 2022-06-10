1536ce655a186fd72265c4b7ebfee725274a5f39
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2022-06-10 11:46:16 +0100

    Improved eslint detection and ignore

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+      "no-unexpected-multiline": "warn",
+      "no-extra-boolean-cast": "warn",
+      "@typescript-eslint/no-inferrable-types": "warn",
+      "@typescript-eslint/no-var-requires": "warn",
+    },
+}
