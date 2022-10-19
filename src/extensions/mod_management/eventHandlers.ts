a5a800d2aaa1b08e43028edef015482d660e72e4
TanninOne <seppleviathan@gmx.de>
2022-10-19 12:51:55 +0200

    fixed: exception removing mods
    not released
    
    fixes #13366

--- src/extensions/mod_management/eventHandlers.ts
+++ src/extensions/mod_management/eventHandlers.ts
@@ -648,7 +648,7 @@ export function onRemoveMods(api: IExtensionApi,
     setModsEnabled(api, profileId, modIds, false, {
       installed: options?.incomplete,
       allowAutoDeploy: false,
-      willBeReplaced: options.willBeReplaced,
+      willBeReplaced: options?.willBeReplaced,
     });
   }
 
