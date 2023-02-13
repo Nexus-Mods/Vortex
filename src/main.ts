059c80e5f628e102a3af80c3ee896efa4bbc2b8d
TanninOne <seppleviathan@gmx.de>
2023-02-13 14:27:11 +0100

    nicer error message if .note file can't be loaded
    if it happens during startup where we can't render dialogs yet

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,27 @@ import { app, dialog } from 'electron';
 import * as path from 'path';
 
 const earlyErrHandler = (error) => {
-  dialog.showErrorBox('Unhandled error',
-    'Vortex failed to start up. This is usually caused by foreign software (e.g. Anti Virus) '
-    + 'interfering.\n\n' + error.stack);
+  if (error.stack.includes('[as dlopen]')) {
+    dialog.showErrorBox(
+      'Vortex failed to start up',
+      `An unexpected error occurred while Vortex was initialising:\n\n${error.message}\n\n`
+
+      + 'This is often caused by a bad installation of the app, '
+      + 'a security app interfering with Vortex '
+      + 'or a problem with the Microsoft Visual C++ Redistributable installed on your PC. '
+      + 'To solve this issue please try the following:\n\n'
+
+      + '- Wait a moment and try starting Vortex again\n'
+      + '- Reinstall Vortex from the Nexus Mods website\n'
+      + '- Install the latest Microsoft Visual C++ Redistributable (find it using a search engine)\n'
+      + '- Disable anti-virus or other security apps that might interfere and install Vortex again\n\n'
+
+      + 'If the issue persists, please create a thread in our support forum for further assistance.');
+  } else {
+    dialog.showErrorBox('Unhandled error',
+      'Vortex failed to start up. This is usually caused by foreign software (e.g. Anti Virus) '
+      + 'interfering.\n\n' + error.stack);
+  }
   app.exit(1);
 };
 
