d3e5b9a7a25bcb36c6199b6c65b9855ed86baf60
TanninOne <seppleviathan@gmx.de>
2023-03-28 17:32:34 +0200

    fixed: error managing a game with no collections
    the api produces an error message if we request the list of own collections for a game that has none

--- src/extensions/nexus_integration/eventHandlers.ts
+++ src/extensions/nexus_integration/eventHandlers.ts
@@ -357,7 +357,7 @@ export function onGetMyCollections(api: IExtensionApi, nexus: Nexus)
       CURRENT_REVISION_INFO, nexusGameId(game), count, offset))
       .then(res => (res ?? []).map(coll => coll.currentRevision))
       .catch(err => {
-        if (err.code !== 'UNAUTHORIZED') {
+        if (!['NOT_FOUND', 'UNAUTHORIZED'].includes(err.code)) {
           api.showErrorNotification('Failed to get list of collections', err);
         }
         return Promise.resolve([]);
