Nexus-Mods/Vortex

commit a9e35e1b7c32076ae6f5484282a2f6a814c3362d

no error if installPathForGame is called with no game active
this is mostly to silence an error from an extension that's probably not going to get fixed but also throwing an exception from selectors generally makes react-redux code more complicated, mapStateToProps should be simple

src/extensions/mod_management/selectors.ts
@@ -29,7 +29,7 @@ export const installPathForGame = createCachedSelector(
     (inPath: string, gameId: string) => getInstallPath(inPath, gameId),
   )((state, gameId) => {
     if (gameId === undefined) {
-      throw new Error('gameId can\'t be undefined');
+      return undefined;
     }
     return gameId;
   });

commit 243a39c3a72b875fe3d2c517cbd2532ae510ec74

unreleased: console log if no game managed
this was caused by a9e35e1b7c32076ae6f5484282a2f6a814c3362d because re-reselect doesn't usually deal well with selectors receiving null or undefined as a key

src/extensions/mod_management/selectors.ts
@@ -8,6 +8,7 @@ import getInstallPath from './util/getInstallPath';
 
 import createCachedSelector from 're-reselect';
 import { createSelector } from 'reselect';
+import { RelaxedReselectCache } from '../../util/RelaxedReselectCache';
 
 const installPathPattern = (state: IState) => state.settings.mods.installPath;
 const gameInstallPathPattern = (state: IState, gameId: string) =>
@@ -26,12 +27,15 @@ export const installPath = createSelector(installPathPattern, activeGameId,
 export const installPathForGame = createCachedSelector(
     gameInstallPathPattern,
     (state: IState, gameId: string) => gameId,
-    (inPath: string, gameId: string) => getInstallPath(inPath, gameId),
+    (inPath: string, gameId: string) =>
+      gameId !== undefined ? getInstallPath(inPath, gameId) : undefined,
   )((state, gameId) => {
     if (gameId === undefined) {
       return undefined;
     }
     return gameId;
+  }, {
+    cacheObject: new RelaxedReselectCache(),
   });
 
 export const currentActivator = createSelector(activators, activeGameId,

src/util/RelaxedReselectCache.ts
@@ -1,0 +1,27 @@
+const NULL_KEY = 'e210e05b-5b22-47ee-96d7-cfd10ae18ef9';
+
+export class RelaxedReselectCache {
+  private mCache: Map<string, any>;
+  constructor() {
+    this.mCache = new Map();
+  }
+  public set(key: string, selectorFn) {
+    this.mCache.set(key ?? NULL_KEY, selectorFn);
+  }
+
+  public get(key: string) {
+    return this.mCache.get(key ?? NULL_KEY);
+  }
+
+  public remove(key: string) {
+    this.mCache.delete(key ?? NULL_KEY);
+  }
+
+  public clear() {
+    this.mCache.clear();
+  }
+
+  public isValidCacheKey() {
+    return true;
+  }
+}