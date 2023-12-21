db44babc7a4faf127bcac6b629350349ccbb328f
madtisa <97251780+madtisa@users.noreply.github.com>
2023-12-21 14:26:03 +0300

    Try to parse version strictly first
    To be able to support prereleases

--- src/extensions/mod_management/util/versionClean.ts
+++ src/extensions/mod_management/util/versionClean.ts
@@ -1,7 +1,8 @@
 import * as semver from 'semver';
 
 function versionClean(input: string): string {
-  let res = semver.valid(semver.parse(input));
+  let res = semver.valid(semver.parse(input) ?? semver.coerce(input));
+  
   if (res !== null) {
     res = semver.clean(res);
   }
