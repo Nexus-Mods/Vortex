e20c1c99c13c4b0b371a2cc10db871f7cd1207c8
insomnious <simon.davies@nexusmods.com>
2024-07-03 10:13:11 +0100

    updated types and tweaked url generation

--- src/extensions/mods_of_the_month_dashlet/types.ts
+++ src/extensions/mods_of_the_month_dashlet/types.ts
@@ -1,10 +1,37 @@
 export interface IMOTMEntry {
+  /**
+   * Generated ID for the video 
+   */
   id: string;
-  link: string;
+  
+  /**
+   * YouTube id of the video
+   */
+  videoid: string;
+
+  /**
+   * timestamp of the video
+   */
   date: number;
 }
 
 export interface IMOTMEntryExt extends IMOTMEntry {
+  
+  /**
+   * Generated YouTube embed link
+   * @example https://www.youtube.com/embed/abc123
+   */
+  link: string;
+  
+  /**
+   * name of the month
+   * @example January
+   */
   month: string;
+  
+  /**
+   * year of the video
+   * @example 2024
+   */
   year: string;
 }
