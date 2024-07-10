431fadac4fd8993d9d7dfc5182cd27fca44c8885
insomnious <simon.davies@nexusmods.com>
2024-07-10 15:56:38 +0100

    bat file exists check

--- test-codesigntool.ps1
+++ test-codesigntool.ps1
@@ -5,7 +5,8 @@ function TestFile {
 
   if (Test-Path $Path -PathType Leaf) {
       Write-Host "File exists: $Path";
-  } else {
+  }
+  else {
       Write-Error "File doesn't exist: $Path";
       exit 1;
   }
@@ -18,25 +19,29 @@ function TestDirectory {
 
   if (Test-Path $Path -PathType Container) {
       Write-Host "Directory exists: $Path";
-  } else {
+  }
+  else {
       Write-Error "Directory doesn't exist: $Path";
       exit 1;
   }
 }
 
-$codeSignToolDir = [IO.Path]::GetFullPath(".\CodeSignTool")
+$codeSignToolDir = [IO.Path]::GetFullPath('.\CodeSignTool')
 TestDirectory($codeSignToolDir)
+
+$codeSignToolBat = Join-Path -Path $codeSignToolDir -ChildPath 'CodeSignTool.bat'
+TestFile($codeSignToolBat)
 
 Set-Location $codeSignToolDir
 
-& "$codeSignToolDir\CodeSignTool.bat" credential_info -username="$env:ES_USERNAME" -password="$env:ES_PASSWORD" -credential_id="$env:ES_CREDENTIAL_ID"
+& $codeSignToolBat credential_info -username="$env:ES_USERNAME" -password="$env:ES_PASSWORD" -credential_id="$env:ES_CREDENTIAL_ID"
 
-Set-Location ".."
 $exitCode = $LASTEXITCODE
 
 if ($exitCode -eq 0) {
-  Write-Host "Signing test completed"
-} else {
+  Write-Host 'Signing test completed'
+}
+else {
   Write-Error "Signing test failed with code $exitCode"
   exit $exitCode
 }
