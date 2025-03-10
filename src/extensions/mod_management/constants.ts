397eec3af01266f28858453001b01e3abbfa3e95
IDCs <idc.dts@gmail.com>
2025-03-10 09:07:29 +0000

    adding instruction override file to deploy blacklist

--- src/extensions/mod_management/constants.ts
+++ src/extensions/mod_management/constants.ts
@@ -1,5 +1,7 @@
 import * as path from 'path';
 
+export const VORTEX_OVERRIDE_INSTRUCTIONS_FILENAME = 'vortex_override_instructions.json';
+
 export const DEPLOY_BLACKLIST: string[] = [
   path.join('**', '.git', '**', '*'),
   path.join('**', '.gitignore'),
@@ -7,9 +9,8 @@ export const DEPLOY_BLACKLIST: string[] = [
   path.join('**', '.gitattributes'),
   path.join('**', 'meta.ini'),
   path.join('**', '_macosx', '**', '*'),
+  path.join('**', VORTEX_OVERRIDE_INSTRUCTIONS_FILENAME),
 ];
 
 export const MIN_VARIANT_NAME = 1;
 export const MAX_VARIANT_NAME = 30;
-
-export const VORTEX_OVERRIDE_INSTRUCTIONS_FILENAME = 'vortex_override_instructions.json';
