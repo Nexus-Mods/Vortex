74b643f3344bf835c2c17632335008abae7f73b9
IDCs <adrianraer@gmail.com>
2025-08-07 06:14:50 +0100

    non-blocking metadata lookup

--- src/extensions/download_management/util/postprocessDownload.ts
+++ src/extensions/download_management/util/postprocessDownload.ts
@@ -1,8 +1,10 @@
 import { IExtensionApi } from '../../../types/IExtensionContext';
 import { delayed, toPromise } from '../../../util/util';
+import { log } from '../../../util/log';
 import { finalizingDownload, finalizingProgress,
          finishDownload, setDownloadHash } from '../actions/state';
 import queryInfo from './queryDLInfo';
+import { batchDispatch } from '../../../util/util';
 
 import { fileMD5 } from 'vortexmt';
 
@@ -24,7 +26,6 @@ export function finalizeDownload(api: IExtensionApi, id: string,
       api.store.dispatch(finalizingProgress(id, progress));
     }
   };
-
   return fileMD5Async(filePath , progressHash)
     .catch(err => {
       if (['EBUSY', 'ENOENT', 'EPERM'].includes(err.code)) {
@@ -33,14 +34,30 @@ export function finalizeDownload(api: IExtensionApi, id: string,
       }
       return Promise.reject(err);
     })
-    .then((md5Hash: string) => {
-      api.store.dispatch(setDownloadHash(id, md5Hash));
+    .then((md5Hash: string) => {;
+      const batched = [
+        setDownloadHash(id, md5Hash),
+        finishDownload(id, 'finished', undefined)
+      ];
+      batchDispatch(api.store, batched);
+      api.events.emit('did-finish-download', id, 'finished');
+
+      // Run metadata lookup asynchronously without blocking download completion
+      queryInfo(api, [id], false).catch(err => {
+        // Log error but don't fail the download
+        log('warn', 'Failed to query download metadata', err.message);
+      });
+      return Promise.resolve();
     })
-    .then(() => queryInfo(api, [id], false))
-    .finally(() => {
-      // still storing the download as successful even if we didn't manage to calculate its
-      // hash
+    .catch(err => {
+      // If MD5 calculation fails, still mark download as finished
       api.store.dispatch(finishDownload(id, 'finished', undefined));
       api.events.emit('did-finish-download', id, 'finished');
+      log('error', 'Failed MD5 calculation', {
+        id,
+        filePath,
+        error: err.message,
+        stack: err.stack
+      });
     });
 }
