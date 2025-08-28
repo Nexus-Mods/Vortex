78165c792aac2ca4ffd42ce69ae975575293c598
Simon Davies <simon.davies@nexusmods.com>
2025-08-28 14:11:37 +0100

    playwright added with initial test

---
+++ playwright.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './playwright/tests',
+  timeout: 60000,
+  outputDir: './playwright/test-results', 
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: [['html', { outputFolder: 'playwright/playwright-report' }]],
+  use: {
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
+  },
+});
