a3ee5ba7fda0175d51a1b426299fa74958aea93e
Simon Davies <simon.davies@nexusmods.com>
2025-09-10 17:03:44 +0100

    remove old metrics, ua and ga4

--- src/extensions/analytics/index.ts
+++ src/extensions/analytics/index.ts
@@ -1,22 +1,13 @@
-import { contextType } from 'react-bootstrap/lib/Accordion';
 import * as semver from 'semver';
 import { IExtensionContext } from '../../types/IExtensionContext';
 import { getApplication } from '../../util/application';
 import { analyticsLog } from './utils/analyticsLog';
-import { activeGameId, discoveryByGame } from '../../util/selectors';
-import { getGame } from '../gamemode_management/util/getGame';
 import { setAnalytics } from './actions/analytics.action';
-import AnalyticsUA, { DIMENSIONS } from './analytics/AnalyticsUA';
-import AnalyticsGA4 from './analytics/AnalyticsGA4';
 import AnalyticsMixpanel from './mixpanel/MixpanelAnalytics';
 import { AppCrashedEvent, AppLaunchedEvent, MixpanelEvent, ModsInstallationCompletedEvent } from './mixpanel/MixpanelEvents';
-import { EVENTS_EVENT_LISTENERS, EVENTS_STATE_LISTENERS } from './analytics/events';
-import { NAVIGATION_EVENT_LISTENERS, NAVIGATION_STATE_LISTENERS } from './analytics/navigation';
 import { HELP_ARTICLE } from './constants';
 import settingsReducer from './reducers/settings.reducer';
 import SettingsAnalytics from './views/SettingsAnalytics';
-import { IMod } from '@nexusmods/nexus-api/lib/types';
-import metrics from './analytics/Metrics';
 
 let ignoreNextAnalyticsStateChange = false;
 
@@ -71,53 +62,20 @@ function init(context: IExtensionContext): boolean {
     const analyticsSettings = ['settings', 'analytics', 'enabled'];
     context.api.onStateChange(analyticsSettings, (oldState, newState) => {
 
-
       if (ignoreNextAnalyticsStateChange) {
         ignoreNextAnalyticsStateChange = false;
         return;
       }
       if (newState) {
         initializeAnalytics();
-        AnalyticsUA.trackClickEvent('Tracking', 'Allow - Settings');
-        AnalyticsGA4.stop();
       } else {
-        AnalyticsUA.trackClickEvent('Tracking', 'Deny - Settings');
-        AnalyticsUA.stop();
         AnalyticsMixpanel.stop();
       }
     });
 
-
-
-
-
-    // new settings events
-
-    // register settings tracking event when analytics is changed
-    context.api.onStateChange(['settings', 'analytics', 'enabled'], (oldState, newState) => {
-      AnalyticsGA4.trackSettingsEvent('Analytics', newState);
-    });
-
-    // register settings tracking event when language is changed
-    context.api.onStateChange(['settings', 'interface', 'language'], (oldState, newState) => {
-      AnalyticsGA4.setUserProperty('Language', newState);
-      AnalyticsGA4.trackSettingsEvent('Language', newState);
-    });
-
-    // register settings tracking event when theme is changed
-    context.api.onStateChange(['settings', 'interface', 'currentTheme'], (oldState, newState) => {
-      AnalyticsGA4.setUserProperty('Theme', newState);
-      AnalyticsGA4.trackSettingsEvent('Theme', newState);
-    });
-
-    
-
-
-
-
-
     // Check for user login
     context.api.onStateChange(['persistent', 'nexus', 'userInfo'], (previous, current) => {
+
       if (enabled() && current) {
         // If the setting is set to true, and I just logged in, skip the Dialog and just turn on Analytics
         initializeAnalytics()
@@ -125,21 +83,19 @@ function init(context: IExtensionContext): boolean {
         // If I was not logged it, and the tracking is undefined ask me for the tracking
         showConsentDialog();
       } else if (!current) {
-        // If I'm logging out disable tracking
-        AnalyticsUA.stop();
-        AnalyticsGA4.stop();
+        // If logging out, disable tracking
         AnalyticsMixpanel.stop();
       }
     });
 
-    // Extra listener in case I need to set a custom navigation,
-    // eg: Custom Modals or custom tabs in the extensions
-    context.api.events.on('analytics-track-navigation', pageId => {
-      AnalyticsUA.trackNavigation(pageId);
-      AnalyticsGA4.trackPageView(pageId);
-      metrics.trackEvent('Navigation', {pageId});
-    });
+    // EVENTS THAT WE NEED TO HUNT DOWN IN CODEBASE
+    // 'analytics-track-navigation'
+    // 'analytics-track-event'
+    // 'analytics-track-event-with-payload'
+    // 'analytics-track-click-event'
 
+
+    // Extra listener in case I need to set a custom navigation,
 
 
     // Mixpanel specific event
@@ -148,53 +104,7 @@ function init(context: IExtensionContext): boolean {
     });
   
 
-
-    // Custom event for event tracking
-    context.api.events.on('analytics-track-event', (category, action, label?, value?) => {
-      AnalyticsUA.trackEvent(category, action, label, value);
-      AnalyticsGA4.trackEvent(action.toLocaleLowerCase(), category, label, value);
-      metrics.trackEvent(action.toLocaleLowerCase(), {category, label, value});
-    });
-
-    // Custom event for event tracking (with raw payload)
-    context.api.events.on('analytics-track-event-with-payload', (action, payload) => {
-      AnalyticsGA4.trackEventWithRawPayload(action.toLocaleLowerCase(), payload);
-      metrics.trackEvent(action.toLocaleLowerCase(), payload);
-    });
-
-    // Custom event for event tracking
-    context.api.events.on('analytics-track-click-event', (category, label?, value?) => {
-      AnalyticsUA.trackClickEvent(category, label, value);
-      AnalyticsGA4.trackClickEvent(category, label, value);
-      metrics.trackEvent('Click', {category, label, value});
-    });
-
-    // Used to ensure a new dimension is set when changing game by restarting the UA tracking
-    // Don't need this for GA4
-    context.api.onStateChange(['settings', 'profiles', 'activeProfileId'], () => {
-      if (AnalyticsUA.isUserSet()) {
-        initializeAnalytics();
-      }
-    });
-
-    // All state listeners
-    const stateListners = [
-      ...NAVIGATION_STATE_LISTENERS,
-      ...EVENTS_STATE_LISTENERS,
-    ];
-    for (const stateListner of stateListners) {
-      context.api.onStateChange(stateListner.path, stateListner.callback);
-    }
-
-    // All event listeners
-    const eventListners = [
-      ...NAVIGATION_EVENT_LISTENERS,
-      ...EVENTS_EVENT_LISTENERS,
-    ];
-    for (const eventListner of eventListners) {
-      context.api.events.on(eventListner.event, eventListner.callback);
-    }
-
+   
     async function initializeAnalytics() {
 
       try {
@@ -205,81 +115,13 @@ function init(context: IExtensionContext): boolean {
 
         const state = context.api.getState();
 
-        const gameId = activeGameId(state);
-        let gameVersion = '';
-        let extensionVersion = '';
-        let gameProfileCount = 0;
-
-        if (gameId) {
-          const game = getGame(gameId);          
-          extensionVersion = game.version;
-          gameVersion = await game.getInstalledVersion(discoveryByGame(state, gameId));
-          gameProfileCount = Object.values(state.persistent.profiles).filter((profile) => { return profile.gameId === gameId }).length;         
-        }
-
-        const theme = state.settings.interface['currentTheme'];
-        const language = state.settings.interface['language'];
-
         // Determine if this is a stable version for analytics routing
         const appVersion = getApplication().version;
         const parsedVersion = semver.parse(appVersion);
         const isStable = parsedVersion && 
                         !parsedVersion.prerelease.length &&
                         appVersion !== '0.0.1' &&
-                        process.env.NODE_ENV !== 'development';
-
-        /**
-        * don't need now that we are forcing users to relogin if older than a certain version
-        * 
-        const apiKey = state.confidential.account?.['nexus']?.['APIKey'];
-        const oauthCred = state.confidential.account?.['nexus']?.['OAuthCredentials'];
-        log('info', 'initializeAnalytics()', { apiKey: apiKey !== undefined, oauthCred: oauthCred !== undefined });
-
-        const authType = oauthCred !== undefined ? 'oauth' : (apiKey !== undefined ? 'apikey' : 'none');
-        */
-
-        const allGameAndMods = state.persistent.mods;
-
-        const gameCount = Object.keys(allGameAndMods).length;
-
-        // reduces array of mods per game into a large array of all mod ids
-        const allModsAndCollections = Object.values(allGameAndMods).reduce(
-          (accumulator, current) => accumulator.concat(Object.values(current)), []); 
-
-        const collectionCount = allModsAndCollections.filter((mod) => { return mod.type === 'collection'}).length;
-        const modCount = allModsAndCollections.filter((mod) => { return mod.type !== 'collection'}).length;
-
-        const membership = userInfo.isPremium
-          ? 'Premium'
-          : userInfo.isSupporter
-            ? 'Supporter'
-            : 'Member';       
-
-        const userProperties = {
-          ["VortexVersion"]: getApplication().version,
-          ["Membership"]: membership,
-          ["Game"]: gameId,
-          ["GameVersion"]: gameVersion,
-          ["GameExtensionVersion"]: extensionVersion,
-          ["Theme"]: theme,
-          ["Sandbox"]: state.settings.mods['installerSandbox'] ?? true,
-          ["ModCount"]: modCount,
-          ["CollectionCount"]: collectionCount,
-          ["GameCount"]: gameCount,
-          ["Language"]: language,
-          ["GameProfileCount"]: gameProfileCount,
-        };
-
-        AnalyticsGA4.start(instanceId, updateChannel, userProperties);
-        
-        AnalyticsUA.start(instanceId, updateChannel, {
-          [DIMENSIONS.VortexVersion]: getApplication().version,
-          [DIMENSIONS.Membership]: membership,
-          [DIMENSIONS.Game]: gameId,
-          [DIMENSIONS.GameVersion]: gameVersion,
-          [DIMENSIONS.Theme]: theme,
-          [DIMENSIONS.Sandbox]: state.settings.mods['installerSandbox'] ?? true,
-        });
+                        process.env.NODE_ENV !== 'development';              
 
         AnalyticsMixpanel.start(userInfo, isStable);
 
@@ -294,13 +136,8 @@ function init(context: IExtensionContext): boolean {
           '12345', // error code - not available yet
           'Simon\'s invented error'   // error message - not available yet
         ));
-
-        metrics.start(instanceId, getApplication().version, context.api);
         
         analyticsLog('info', 'Analytics initialized');
-        analyticsLog('debug', `User properties for channel: ${updateChannel}`, userProperties); 
-
-        AnalyticsUA.trackEvent('Vortex', 'Version', getApplication().version);
 
       } catch (err) {
         // there is no error handling anywhere invoking initializeAnalytics,
@@ -337,8 +174,6 @@ function init(context: IExtensionContext): boolean {
                   dismiss();
                   if (result.action === 'Allow') {
                     initializeAnalytics();
-                    AnalyticsUA.trackClickEvent('Tracking', 'Allow');
-                    AnalyticsGA4.trackClickEvent('Tracking', 'Allow');
                     ignoreNextAnalyticsStateChange = true;
                     context.api.store.dispatch(setAnalytics(true));
                   } else if (result.action === 'Deny') {
