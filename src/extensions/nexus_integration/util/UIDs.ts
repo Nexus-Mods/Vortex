f7b808995b0cab73f592299eeae4768c12c072e5
Simon Davies <simon.davies@nexusmods.com>
2025-09-18 11:21:27 +0100

    fix for uid creation

--- src/extensions/nexus_integration/util/UIDs.ts
+++ src/extensions/nexus_integration/util/UIDs.ts
@@ -16,17 +16,34 @@ const gameNum = (() => {
 })();
 
 export function makeFileUID(repoInfo: IModRepoId): string {
-  return ((BigInt(gameNum(repoInfo.gameId)) << BigInt(32))
+  
+  // check if gameId is numeric and if not, use gameNum() to convert
+  const gameIdNum = /^\d+$/.test(repoInfo.gameId)
+    ? parseInt(repoInfo.gameId, 10)
+    : gameNum(repoInfo.gameId);
+
+  return ((BigInt(gameIdNum) << BigInt(32))
     | BigInt(parseInt(repoInfo.fileId, 10))).toString();
 }
 
 export function makeModUID(repoInfo: IModRepoId): string {
-  return ((BigInt(gameNum(repoInfo.gameId)) << BigInt(32))
+
+  // check if gameId is numeric and if not, use gameNum() to convert
+  const gameIdNum = /^\d+$/.test(repoInfo.gameId)
+    ? parseInt(repoInfo.gameId, 10)
+    : gameNum(repoInfo.gameId);
+
+  return ((BigInt(gameIdNum) << BigInt(32))
     | BigInt(parseInt(repoInfo.modId, 10))).toString();
 }
 
 export function makeModAndFileUIDs(gameId: string, modId: string, fileId: string): { modUID: string, fileUID: string } {
+
+  // 1303 518 138454
+
   const repoInfo = { gameId, modId, fileId };
+  console.log('makeModAndFileUIDs', gameId, modId, fileId);
+  console.log('makeModAndFileUIDs repoInfo', repoInfo);
   return {
     modUID: makeModUID(repoInfo),
     fileUID: makeFileUID(repoInfo)
