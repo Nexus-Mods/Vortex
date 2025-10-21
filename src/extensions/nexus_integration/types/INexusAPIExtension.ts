14d41128a2e73e27d728fe64418176c3b2d59050
Simon Davies <simon.davies@nexusmods.com>
2025-10-21 16:41:46 +0100

    wip nexus integration search stuff

--- src/extensions/nexus_integration/types/INexusAPIExtension.ts
+++ src/extensions/nexus_integration/types/INexusAPIExtension.ts
@@ -1,4 +1,4 @@
-import { EndorsedStatus, ICollection, ICollectionManifest, IDownloadURL, IFeedbackResponse, IFileInfo, IIssue, IRevision } from '@nexusmods/nexus-api';
+import { EndorsedStatus, ICollection, ICollectionManifest, ICollectionQuery, IDownloadURL, IFeedbackResponse, IFileInfo, IIssue, IRevision } from '@nexusmods/nexus-api';
 import { IMod } from '../../mod_management/types/IMod';
 
 export type SortField = 'createdAt' | 'endorsements' | 'recentRating' | 'downloads';
@@ -27,7 +27,7 @@ export interface INexusAPIExtension {
       => PromiseLike<string>;
   nexusGetCollection?: (slug: string) => PromiseLike<ICollection>;
   nexusGetCollections?: (gameId: string) => PromiseLike<ICollection[]>;
-  nexusSearchCollections?: (options: ICollectionSearchOptions) => PromiseLike<ICollectionSearchResult>;
+  nexusSearchCollections?: (query: ICollectionQuery, options: ICollectionSearchOptions) => PromiseLike<ICollectionSearchResult>;
   nexusGetMyCollections?:
     (gameId: string, count?: number, offset?: number) => PromiseLike<IRevision[]>;
   nexusResolveCollectionUrl?: (apiLink: string) => PromiseLike<IDownloadURL[]>;
