55b1f8870cb4c12df150e3b1c634e35a0d68299b
Simon Davies <simon.davies@nexusmods.com>
2025-10-23 07:53:55 +0100

    added input and select from next

---
+++ src/tailwind/components/next/form/select/Select.tsx
@@ -0,0 +1,79 @@
+/**
+ * Select Component
+ * Adapted from web team's "next" project for Vortex
+ *
+ * Provides a styled select dropdown with custom icon and validation support.
+ */
+
+import * as React from 'react';
+import { mdiMenuDown } from '@mdi/js';
+import { Ref, SelectHTMLAttributes } from 'react';
+
+import { Icon } from '../../icon';
+import { joinClasses } from '../../utils';
+import { BaseFormFieldProps, FormField } from '../formfield';
+
+export type SelectProps = SelectHTMLAttributes<HTMLSelectElement> &
+  BaseFormFieldProps & {
+    ref?: Ref<HTMLSelectElement>;
+  };
+
+export const Select = ({
+  children,
+  className,
+  disabled,
+  errorMessage,
+  hideLabel = false,
+  hints = [],
+  id,
+  label = '',
+  ref,
+  required,
+  showRequiredLabel,
+  ...props
+}: SelectProps) => {
+  // Make sure hints is always an array
+  hints = !Array.isArray(hints) ? [hints] : hints;
+
+  return (
+    <FormField
+      className={className}
+      disabled={disabled}
+      errorMessage={errorMessage}
+      hideLabel={hideLabel}
+      hints={hints}
+      id={id}
+      label={label}
+      showRequiredLabel={showRequiredLabel ?? required}
+    >
+      <div className={joinClasses(['tw:relative tw:flex tw:flex-col', className], { 'tw:opacity-40': disabled })}>
+        <select
+          {...props}
+          ref={ref}
+          aria-describedby={!!errorMessage ? `${id}_error` : hints.length > 0 ? `${id}_hints` : undefined}
+          aria-invalid={!!errorMessage || undefined}
+          className={joinClasses([
+            'tw:border-neutral-subdued tw:bg-surface-low tw:text-base tw:text-neutral-strong tw:group tw:relative tw:w-full tw:appearance-none tw:rounded tw:border tw:py-2 tw:pl-4 tw:pr-12',
+            !!errorMessage ? 'tw:border-danger-strong tw:hover:border-neutral-subdued' : 'tw:border-neutral-subdued',
+            'tw:hover:border-white',
+            'tw:focus:bg-surface-low tw:focus:border-white tw:focus-visible:outline-offset-1',
+          ])}
+          disabled={disabled}
+          id={id}
+          required={required}
+        >
+          {children}
+        </select>
+
+        <Icon
+          className={joinClasses([
+            'tw:text-neutral-subdued tw:pointer-events-none tw:absolute tw:right-4 tw:top-1/2 tw:-translate-y-1/2 tw:transform',
+            'tw:group-hover:text-white tw:group-focus:text-white',
+          ])}
+          path={mdiMenuDown}
+          size="lg"
+        />
+      </div>
+    </FormField>
+  );
+};
