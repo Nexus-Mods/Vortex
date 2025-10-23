2771735631922a2083689823229b7373d537cef5
IDCs <adrianraer@gmail.com>
2025-10-23 07:04:25 +0100

    fixed instructions overrides not working correctly for gamebryo games
    fixes nexus-mods/vortex#18593

--- src/extensions/installer_fomod/util/gameSupport.ts
+++ src/extensions/installer_fomod/util/gameSupport.ts
@@ -28,7 +28,7 @@ const gamebryoPatterns: string[] = [
   'fomod/ModuleConfig.xml$',
 ].concat(gamebryoTopLevel.map(toWordExp));
 
-const uniPatterns: string[] = ['fomod'].map(toWordExp);
+export const uniPatterns: string[] = ['fomod'].map(toWordExp);
 
 function stopPatterns(gameMode: string) {
   switch (gameMode) {
