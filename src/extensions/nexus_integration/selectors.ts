6f6621cf62e639006764065348e62a7238fe2817
IDCs <adrianraer@gmail.com>
2025-11-06 15:13:08 +0000

    fixing nexusIds selector

--- src/extensions/nexus_integration/selectors.ts
+++ src/extensions/nexus_integration/selectors.ts
@@ -27,8 +27,8 @@ export const nexusIdsFromDownloadId = createSelector(
     const numericGameId = nexusGames().find(g => g.domain_name === (dl.modInfo?.nexus?.ids?.gameId || dl?.modInfo?.meta?.domainName));
     return {
       gameDomainName: dl?.modInfo?.nexus?.ids?.gameId || dl?.modInfo?.meta?.domainName,
-      fileId: dl?.modInfo?.nexus?.ids?.fileId.toString(),
-      modId: dl?.modInfo?.nexus?.ids?.modId.toString(),
+      fileId: dl?.modInfo?.nexus?.ids?.fileId?.toString(),
+      modId: dl?.modInfo?.nexus?.ids?.modId?.toString(),
       numericGameId: numericGameId?.id || parseInt(dl?.modInfo?.meta?.gameId),
       collectionSlug: dl?.modInfo?.nexus?.ids?.collectionSlug,
       collectionId: dl?.modInfo?.nexus?.ids?.collectionId?.toString() ?? dl?.modInfo?.nexus?.revisionInfo?.collection?.id?.toString(),
