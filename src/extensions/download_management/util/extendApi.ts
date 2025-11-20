1507edcf90db740627883a704491fd96d6852c5e
IDCs <adrianraer@gmail.com>
2025-11-20 10:03:32 +0000

    adding api extension to wrap download observer event calls

---
+++ src/extensions/download_management/util/extendApi.ts
@@ -0,0 +1,63 @@
+import { IExtensionApi } from '../../../types/IExtensionContext';
+import { IDownloadsAPIExtension } from '../types/IDownloadsAPIExtension';
+import { IDownloadRemoveOptions } from '../types/IDownloadRemoveOptions';
+import { IDownloadResult } from '../types/IDownloadResult';
+import { RedownloadMode } from '../DownloadManager';
+import { IStartDownloadOptions } from '../types/IStartDownloadOptions';
+
+function extendAPI(api: IExtensionApi): IDownloadsAPIExtension {
+  return {
+    removeDownload: async (downloadId: string, options?: IDownloadRemoveOptions): Promise<void> => {
+      return new Promise((resolve, reject) => {
+        api.events.emit('remove-download', downloadId, (err: Error) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }, options);
+      });
+    },
+    pauseDownload: async (downloadId: string): Promise<void> => {
+      return new Promise((resolve, reject) => {
+        api.events.emit('pause-download', downloadId, (err: Error) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+    },
+    resumeDownload: async (downloadId: string, options?: IStartDownloadOptions): Promise<void> => {
+      return new Promise((resolve, reject) => {
+        api.events.emit('resume-download', downloadId, (err: Error) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }, options);
+      });
+    },
+    startDownload: async (
+      urls: string[],
+      modInfo: any,
+      fileName: string,
+      redownload?: RedownloadMode,
+      options?: IStartDownloadOptions
+    ): Promise<IDownloadResult> => {
+      return new Promise((resolve, reject) => {
+        api.events.emit('start-download', urls, modInfo, fileName, (err: Error, result: IDownloadResult) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        }, redownload, options);
+      });
+    }
+  };
+}
+
+export default extendAPI;
