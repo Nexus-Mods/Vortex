0aef5b918e9fe7bd009c046bf5572405bcff460c
Vitalii Mikhailov <personal@aragas.org>
2025-11-25 14:01:35 +0200

    IPC's reportError is passing a single object, not array of args

--- src/extensions/installer_fomod_ipc/installer.ts
+++ src/extensions/installer_fomod_ipc/installer.ts
@@ -57,7 +57,7 @@ export const install = async (
     connection.registerCallback('getExistingDataFileList', (folderPath: string, searchFilter: string, isRecursive: boolean) => csharpDelegates.getExistingDataFileList(folderPath, searchFilter, isRecursive));
     connection.registerCallback('getIniString', (iniFileName: string, section: string, key: string) => csharpDelegates.getIniString(iniFileName, section, key));
     connection.registerCallback('getIniInt', (iniFileName: string, section: string, key: string) => csharpDelegates.getIniInt(iniFileName, section, key));
-    connection.registerCallback('reportError', (title: string, message: string, details: string) => csharpDelegates.reportError(title, message, details));
+    connection.registerCallback('reportError', ({ title, message, details }: { title: string, message: string, details: string }) => csharpDelegates.reportError(title, message, details));
 
     // When override instructions file is present, use only the universal stop patterns and null pluginPath
     // to prevent any automatic path manipulation (both FindPathPrefix and pluginPath stripping)
