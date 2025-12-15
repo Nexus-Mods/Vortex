5f79c5013e2b4d5964529213a947fa600b2ac55c
Sewer56 <sewer56lol@googlemail.com>
2025-12-15 12:00:21 +0000

    Added: Nix Flake and Contributing Guidelines

--- CONTRIBUTE.md
+++ CONTRIBUTE.md
@@ -1,15 +1,18 @@
 # Contributing
 
-## Linux
+## Generic Linux
 
 ### Requirements
 
 - editor
 - git
 - [`volta`](https://volta.sh/)
-- for [node-gyp](https://github.com/nodejs/node-gyp?tab=readme-ov-file#on-unix):
-    - [A supported version of Python](https://devguide.python.org/versions/)
-    - `make` and a C/C++ toolchain (`build-essentials` on Debian/Ubuntu)
+    - `curl https://get.volta.sh | bash`
+
+For [node-gyp](https://github.com/nodejs/node-gyp?tab=readme-ov-file#on-unix):
+
+- A [supported 3.x version of Python](https://devguide.python.org/versions/)
+- `make` and a C/C++ toolchain (`build-essentials` on Debian/Ubuntu)
 - [.NET 9](https://dotnet.microsoft.com/en-us/download)
 
 ### Setup
@@ -22,3 +25,37 @@
 
 To get `nxm` protocol handler to work you need to run the script `./scripts/linux-protocol-registration.sh`. This script only needs to be run once.
 
+### Notes
+
+- In unlikely event you can't run `volta` after install; add it to `$PATH` manually
+    - `export PATH="$HOME/.volta/bin:$PATH"` in `.zshrc`/`.bashrc` etc.
+- You might want to pin node in volta e.g. `volta pin node@22.12.0`, to match what's in `package.json`.
+
+## NixOS
+
+There is a `flake.nix` that provides all required dependencies: `node`, `yarn`, `python`, `make`, `clang` toolchain, and Electron runtime libraries.
+
+### Requirements
+
+- Nix with flakes enabled
+- (Recommended) [direnv](https://github.com/nix-community/nix-direnv) + [VSCode direnv extension](https://marketplace.visualstudio.com/items?itemName=mkhl.direnv)
+
+```nix
+{...}: {
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+}
+```
+
+### Setup
+
+- Clone repository
+- Enter development shell: `nix develop`
+    - Or if using direnv: `direnv allow` (from now on shell will auto-activate)
+- Install dependencies: `yarn install`
+- Build: `yarn build`
+- Start: `yarn start`
---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "Vortex development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Node.js and package manager
+            nodejs_22
+            yarn
+
+            # Python with setuptools for node-gyp (distutils removed in Python 3.12+)
+            (python3.withPackages (ps: [ ps.setuptools ]))
+
+            # Build tools
+            gnumake
+            pkg-config
+
+            # C/C++ toolchain
+            clang
+            llvmPackages.libcxx
+
+            # Electron (wrapped with GTK dependencies)
+            electron_37
+
+            # GTK dependencies for Electron runtime
+            gtk3
+            gtk4
+            glib
+            gsettings-desktop-schemas
+            dconf
+            librsvg
+          ];
+
+          env = {
+            # Compiler settings for node-gyp
+            CC = "${pkgs.clang}/bin/clang";
+            CXX = "${pkgs.clang}/bin/clang++";
+
+            # Ignore strict node engine version checks
+            YARN_IGNORE_ENGINES = "true";
+
+            # Prevent yarn from downloading Electron binaries
+            ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+
+            # Point to Nix-provided Electron
+            ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron_37}/libexec/electron";
+          };
+
+          # Set up GTK environment (mimics wrapGAppsHook3)
+          shellHook = ''
+            # GSettings schemas
+            export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:${pkgs.gtk4}/share/gsettings-schemas/${pkgs.gtk4.name}:${pkgs.glib}/share:$XDG_DATA_DIRS"
+
+            # GIO modules (for dconf)
+            export GIO_EXTRA_MODULES="${pkgs.dconf.lib}/lib/gio/modules"
+
+            # GDK pixbuf loaders (for image loading)
+            export GDK_PIXBUF_MODULE_FILE="${pkgs.librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+
+            # Chromium sandbox
+            export CHROME_DEVEL_SANDBOX="${pkgs.electron_37}/libexec/electron/chrome-sandbox"
+          '';
+        };
+      });
+}
