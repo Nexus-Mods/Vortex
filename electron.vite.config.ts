28247042c1a8305400c726efe2aa30dbe2b79ed5
erri120 <erri120@fossmailer.de>
2026-01-14 16:20:40 +0100

    Add alex8088/electron-vite

---
+++ electron.vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'electron-vite';
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  main: {
+    build: {
+      lib: {
+        entry: "./src/main.ts",
+        formats: ["cjs"]
+      },
+      rollupOptions: {
+        external: ["original-fs"]
+      }
+    }
+  },
+  preload: {
+    build: {
+      lib: {
+        entry: ""
+      }
+    }
+  },
+  renderer: {
+    root: "./src",
+    build: {
+      rollupOptions: {
+        input: "./src/index.html",
+      }
+    },
+    plugins: [react()]
+  }
+});
+
