d730238f5fd28570c1c9fb649319a1062d57e88a
erri120 <erri120@fossmailer.de>
2026-01-15 12:41:09 +0100

    Make Node happy

--- src/renderer/datelocales.ts
+++ src/renderer/datelocales.ts
@@ -1,36 +1,34 @@
-import {
-  af,
-  arSA as ar_sa,
-  bn,
-  de,
-  el,
-  enCA as en_ca,
-  enGB as en_gb,
-  enUS as en_us,
-  eo,
-  es,
-  et,
-  fi,
-  fr,
-  gl,
-  he,
-  hu,
-  id,
-  is,
-  it,
-  lt,
-  nb,
-  nl,
-  pt,
-  ptBR as pt_br,
-  ro,
-  ru,
-  sv,
-  th,
-  uk,
-  vi,
-  zhCN as zh_cn,
-} from "date-fns/locale";
+import af from "date-fns/esm/locale/af/index.js";
+import ar_sa from "date-fns/esm/locale/ar-SA/index.js";
+import bn from "date-fns/esm/locale/bn/index.js";
+import de from "date-fns/esm/locale/de/index.js";
+import el from "date-fns/esm/locale/el/index.js";
+import en_ca from "date-fns/esm/locale/en-CA/index.js";
+import en_gb from "date-fns/esm/locale/en-GB/index.js";
+import en_us from "date-fns/esm/locale/en-US/index.js";
+import eo from "date-fns/esm/locale/eo/index.js";
+import es from "date-fns/esm/locale/es/index.js";
+import et from "date-fns/esm/locale/et/index.js";
+import fi from "date-fns/esm/locale/fi/index.js";
+import fr from "date-fns/esm/locale/fr/index.js";
+import gl from "date-fns/esm/locale/gl/index.js";
+import he from "date-fns/esm/locale/he/index.js";
+import hu from "date-fns/esm/locale/hu/index.js";
+import id from "date-fns/esm/locale/id/index.js";
+import is from "date-fns/esm/locale/is/index.js";
+import it from "date-fns/esm/locale/it/index.js";
+import lt from "date-fns/esm/locale/lt/index.js";
+import nb from "date-fns/esm/locale/nb/index.js";
+import nl from "date-fns/esm/locale/nl/index.js";
+import pt from "date-fns/esm/locale/nl/index.js";
+import pt_br from "date-fns/esm/locale/pt-BR/index.js";
+import ro from "date-fns/esm/locale/ro/index.js";
+import ru from "date-fns/esm/locale/ru/index.js";
+import sv from "date-fns/esm/locale/sv/index.js";
+import th from "date-fns/esm/locale/th/index.js";
+import uk from "date-fns/esm/locale/uk/index.js";
+import vi from "date-fns/esm/locale/vi/index.js";
+import zh_cn from "date-fns/esm/locale/zh-CN/index.js";
 
 const langMap = {
   af,
