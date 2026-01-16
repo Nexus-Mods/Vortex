fcec785680d5e775e787707b02a386d3d39ef489
Blaize Jenkins <jenkinsblaize@yahoo.com>
2026-01-16 08:45:41 -0800

    Name length longer
    MAX_COLLECTION_NAME_LENGTH from 36 to 64

--- extensions/collections/src/constants.ts
+++ extensions/collections/src/constants.ts
@@ -32,7 +32,7 @@ export const INSTALLING_NOTIFICATION_ID = "installing-collection-";
 
 // limits
 export const MIN_COLLECTION_NAME_LENGTH = 3;
-export const MAX_COLLECTION_NAME_LENGTH = 36;
+export const MAX_COLLECTION_NAME_LENGTH = 64;
 
 export const INI_TWEAKS_PATH = "Ini Tweaks";
 
