cd2288ec45e91c377387cf42e836bb99bdef9d16
erri120 <erri120@fossmailer.de>
2026-01-27 13:30:48 +0100

    Formatting

--- typings.custom/index.d.ts
+++ typings.custom/index.d.ts
@@ -1,10 +1,10 @@
-/// <reference path="bbcode-to-react.d.ts" />
-/// <reference path="electron-edge.d.ts" />
-/// <reference path="electron.d.ts" />
-/// <reference path="node-rest-client.d.ts" />
-/// <reference path="promise.d.ts" />
-/// <reference path="redux-devtools-dispatch.d.ts" />
-/// <reference path="redux-persist.d.ts" />
-/// <reference path="redux-watcher.d.ts" />
-/// <reference path="various-ext.d.ts" />
-/// <reference path="simple-vdf.d.ts" />
+import "./bbcode-to-react";
+import "./electron";
+import "./electron-edge";
+import "./node-rest-client";
+import "./promise";
+import "./redux-devtools-dispatch";
+import "./redux-persist";
+import "./redux-watcher";
+import "./simple-vdf";
+import "./various-ext";
