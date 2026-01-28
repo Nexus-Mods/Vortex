8a694dfa6dd90abbc0096afb0a07562698b3cccd
Rich Moore <rich.moore@nexusmods.com>
2026-01-28 15:30:54 +0000

    chore: header ui

--- src/renderer/views/components/Header.tsx
+++ src/renderer/views/components/Header.tsx
@@ -1,6 +1,13 @@
-import { mdiMenuOpen, mdiMenuClose } from "@mdi/js";
+import {
+  mdiMenuOpen,
+  mdiMenuClose,
+  mdiWindowMinimize,
+  mdiWindowMaximize,
+  mdiWindowClose,
+} from "@mdi/js";
 import React from "react";
 
+import { Button } from "../../../tailwind/components/next/button";
 import { Icon } from "../../../tailwind/components/next/icon";
 import { Typography } from "../../../tailwind/components/next/typography";
 import { useWindowContext } from "../../../util/WindowContext";
@@ -9,7 +16,7 @@ export const Header = () => {
   const { isMenuOpen, setIsMenuOpen } = useWindowContext();
 
   return (
-    <div className="flex h-12 items-center justify-between py-3 pr-6 pl-5.5">
+    <div className="flex h-12 items-center justify-between px-6 py-3">
       <div className="flex items-center gap-x-2.5">
         <button
           className="shrink-0 text-neutral-subdued transition-colors hover:text-neutral-moderate"
@@ -25,7 +32,30 @@ export const Header = () => {
         )}
       </div>
 
-      <div>Header stuff</div>
+      <div>
+        <div className="flex gap-x-6">
+          <Button
+            buttonType="tertiary"
+            leftIconPath={mdiWindowMinimize}
+            size="sm"
+            title="Minimize"
+          />
+
+          <Button
+            buttonType="tertiary"
+            leftIconPath={mdiWindowMaximize}
+            size="sm"
+            title="Maximize"
+          />
+
+          <Button
+            buttonType="tertiary"
+            leftIconPath={mdiWindowClose}
+            size="sm"
+            title="Close"
+          />
+        </div>
+      </div>
     </div>
   );
 };
