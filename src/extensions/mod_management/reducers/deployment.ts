8261cf14c131b7db09cb60d2219b9751a1afda0c
IDCs <adrianraer@gmail.com>
2026-02-02 15:18:54 +0000

    fixed tools availability not updating on purge/deploy
    fixes nexus-mods/vortex#19479

--- src/extensions/mod_management/reducers/deployment.ts
+++ src/extensions/mod_management/reducers/deployment.ts
@@ -1,13 +1,25 @@
 import type { IReducerSpec } from "../../../types/IExtensionContext";
 import { setSafe } from "../../../util/storeHelper";
 import * as actions from "../actions/deployment";
+import { createAction } from "redux-act";
+
+// Action to increment deployment counter after deployment completes
+export const incrementDeploymentCounter = createAction(
+  "INCREMENT_DEPLOYMENT_COUNTER",
+  (gameId: string) => ({ gameId }),
+);
 
 export const deploymentReducer: IReducerSpec = {
   reducers: {
     [actions.setDeploymentNecessary as any]: (state, payload) =>
       setSafe(state, ["needToDeploy", payload.gameId], payload.required),
+    [incrementDeploymentCounter as any]: (state, payload) => {
+      const current = state.deploymentCounter?.[payload.gameId] || 0;
+      return setSafe(state, ["deploymentCounter", payload.gameId], current + 1);
+    },
   },
   defaults: {
     needToDeploy: {},
+    deploymentCounter: {},
   },
 };
