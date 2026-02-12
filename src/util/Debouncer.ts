011978d068845962a9acf8d8ac93429a934d28f5
IDCs <adrianraer@gmail.com>
2026-02-12 11:46:59 +0000

    Revert "Merge pull request #19806 from Nexus-Mods/task/18921-main-utils-imports-6"
    temporarily reverting
    
    This reverts commit 7dcfd9f98b6ddf9116a37d721d60b2444d002d3c, reversing
    changes made to d654365c6883ec6a2c70edeaedefa82502b75ae3.

--- src/util/Debouncer.ts
+++ src/util/Debouncer.ts
@@ -1,23 +1,220 @@
-import { GenericDebouncer } from "../shared/Debouncer";
+import { unknownToError } from "../shared/errors";
 
-export default class Debouncer extends GenericDebouncer<
-  ReturnType<typeof setTimeout>,
-  typeof setTimeout,
-  typeof clearTimeout
-> {
+type Callback = (err: Error | null) => void;
+
+/**
+ * management function. Prevents a function from being called too often
+ * and, for function returning a promise, it ensures that it's not run
+ * again (through this Debouncer) before the promise is resolved.
+ *
+ * @class Debouncer
+ */
+class Debouncer {
+  private mDebounceMS: number;
+  private mFunc: (...args: any[]) => Error | PromiseLike<void>;
+  private mTimer: NodeJS.Timeout | undefined;
+
+  private mCallbacks: Callback[] = [];
+  private mAddCallbacks: Callback[] = [];
+  private mRunning: boolean = false;
+  private mReschedule: "no" | "yes" | "immediately" = "no";
+  private mArgs: any[] = [];
+  private mResetting: boolean;
+  private mTriggerImmediately: boolean;
+  // only used with triggerImmediately
+  private mRetrigger: boolean = false;
+
+  /**
+   * constructor
+   * @param func the function to call when the timer expired
+   * @param debounceMS the (minimum) time between two calls
+   * @param reset if true (the default) the time is reset with every
+   *              time schedule gets called. This means if the debouncer
+   *              is triggered regularly in less than debounceMS it never
+   *              gets run.
+   * @param triggerImmediately if true, the debouncer will trigger immediately
+   *                           when first called and then not be called again
+   *                           until the timer expires. Otherwise (the default)
+   *                           the initial call is delay.
+   */
   constructor(
     func: (...args: any[]) => Error | PromiseLike<void>,
     debounceMS: number,
     reset?: boolean,
     triggerImmediately: boolean = false,
   ) {
-    super(
-      setTimeout,
-      clearTimeout,
-      func,
-      debounceMS,
-      reset,
-      triggerImmediately,
-    );
+    this.mResetting = reset !== false;
+    this.mFunc = func;
+    this.mDebounceMS = debounceMS;
+    this.mTriggerImmediately = triggerImmediately;
+  }
+
+  /**
+   * schedule the function and invoke the callback once that is done
+   * @param callback the callback to invoke upon completion
+   * @param args the arguments to pass to the function. When the timer expires
+   *             and the function actually gets invoked, only the last set of
+   *             parameters will be used
+   */
+  public schedule(callback?: (err: Error) => void, ...args: any[]) {
+    if (callback !== undefined && callback !== null) {
+      this.mCallbacks.push(callback);
+    }
+    this.mArgs = args;
+    if (this.mTriggerImmediately && this.mTimer === undefined) {
+      this.run();
+    } else {
+      const doReset = this.mTimer !== undefined && this.mResetting;
+      if (doReset) {
+        this.clear();
+      }
+
+      if (this.mTriggerImmediately) {
+        this.mRetrigger = true;
+        if (doReset) {
+          this.startTimer();
+        }
+      } else if (this.mRunning) {
+        if (this.mReschedule !== "immediately") {
+          this.mReschedule = "yes";
+        }
+      } else if (this.mTimer === undefined) {
+        this.startTimer();
+      }
+    }
+  }
+
+  /**
+   * run the function immediately without waiting for the timer
+   * to run out. (It does cancel the timer though and invokes all
+   * scheduled callbacks)
+   *
+   * @param {(err: Error) => void} callback
+   * @param {...any[]} args
+   *
+   * @memberOf Debouncer
+   */
+  public runNow(callback: (err: Error) => void, ...args: any[]) {
+    if (this.mTimer !== undefined) {
+      this.clear();
+    }
+
+    if (callback !== undefined && callback !== null) {
+      this.mCallbacks.push(callback);
+    }
+
+    this.mArgs = args;
+
+    if (this.mRunning) {
+      this.mReschedule = "immediately";
+    } else {
+      this.run();
+    }
+  }
+
+  /**
+   * wait for the completion of the current timer without scheduling it.
+   * if the function is not scheduled currently the callback will be
+   * called (as a success) immediately.
+   * This does not reset the timer
+   *
+   * @param {(err: Error) => void} callback
+   * @param {boolean} immediately if set (default is false) the function gets called
+   *                              immediately instead of awaiting the timer
+   *
+   * @memberOf Debouncer
+   */
+  public wait(
+    callback: (err: Error | null) => void,
+    immediately: boolean = false,
+  ) {
+    if (this.mTimer === undefined && !this.mRunning) {
+      // not scheduled
+      return callback(null);
+    }
+
+    this.mAddCallbacks.push(callback);
+
+    if (immediately && !this.mRunning) {
+      this.clear();
+
+      this.run();
+    }
+  }
+
+  public clear() {
+    clearTimeout(this.mTimer);
+    this.mTimer = undefined;
+  }
+
+  private run() {
+    const callbacks = this.mCallbacks;
+    this.mCallbacks = [];
+    const args = this.mArgs;
+    this.mArgs = [];
+    this.mTimer = undefined;
+
+    let prom: Error | PromiseLike<void>;
+    try {
+      prom = this.mFunc(...args);
+    } catch (err) {
+      prom = unknownToError(err);
+    }
+
+    if (prom?.["then"] !== undefined) {
+      this.mRunning = true;
+      prom["then"](() => this.invokeCallbacks(callbacks, null))
+        .catch((err: Error) => this.invokeCallbacks(callbacks, err))
+        .finally(() => {
+          this.mRunning = false;
+          if (this.mReschedule === "immediately") {
+            this.mReschedule = "no";
+            this.run();
+          } else if (this.mReschedule === "yes") {
+            this.mReschedule = "no";
+            this.reschedule();
+          }
+        });
+    } else {
+      this.invokeCallbacks(callbacks, prom as Error);
+    }
+
+    // in the default case the "run" marks the end of the timer,
+    // in the "trigger immediately" case it marks the start
+    if (this.mTriggerImmediately) {
+      this.startTimer();
+    }
+  }
+
+  private reschedule() {
+    if (this.mTimer !== undefined && this.mResetting) {
+      this.clear();
+    }
+
+    if (this.mTimer === undefined) {
+      this.startTimer();
+    }
+  }
+
+  private invokeCallbacks(localCallbacks: Callback[], err: Error | null) {
+    localCallbacks.forEach((cb) => cb(err));
+    this.mAddCallbacks.forEach((cb) => cb(err));
+    this.mAddCallbacks = [];
+  }
+
+  private startTimer() {
+    this.mTimer = setTimeout(() => {
+      this.mTimer = undefined;
+      if (!this.mTriggerImmediately || this.mRetrigger) {
+        this.mRetrigger = false;
+        if (this.mRunning) {
+          this.mReschedule = "immediately";
+        } else {
+          this.run();
+        }
+      }
+    }, this.mDebounceMS);
   }
 }
+
+export default Debouncer;
