6c62d8ef0c21e003bb57e77e6bfda686eaf312c8
IDCs <adrianraer@gmail.com>
2026-02-16 15:11:38 +0000

    fixed race condition causing file assembler to close file prematurely

--- src/extensions/download_management/DownloadManager.ts
+++ src/extensions/download_management/DownloadManager.ts
@@ -821,6 +821,7 @@ class DownloadWorker {
     // Reset network retry counter on successful chunk completion
     this.mNetworkRetries = 0;
     this.writeBuffer(str)
+      .then(() => this.waitForInFlightWrites())
       .then(() => {
         if (this.mJob.completionCB !== undefined) {
           this.mJob.completionCB();
@@ -830,6 +831,20 @@ class DownloadWorker {
       .catch(UserCanceled, () => null)
       .catch(ProcessCanceled, () => null)
       .catch((err) => this.handleError(err));
+  };
+
+  private waitForInFlightWrites = (): Bluebird<void> => {
+    if (this.mInFlightWrites <= 0) {
+      return Bluebird.resolve();
+    }
+    return new Bluebird<void>((resolve) => {
+      const check = setInterval(() => {
+        if (this.mInFlightWrites <= 0) {
+          clearInterval(check);
+          resolve();
+        }
+      }, 10);
+    });
   };
 
   private handleResponse = (response: http.IncomingMessage, jobUrl: string) => {
@@ -1010,14 +1025,7 @@ class DownloadWorker {
           this.mInFlightWrites = 0;
         }
 
-        // If the worker has ended, a new worker may already be using this job.
-        // Don't update confirmed fields or the new worker's offset tracking
-        // will be corrupted by this stale confirmation.
-        if (this.mEnded) {
-          return;
-        }
-
-        // Write confirmed - update confirmed received counter
+        // Write confirmed - update job fields from confirmed state only
         this.mJob.confirmedReceived += len;
 
         // Recalculate confirmed-based fields
@@ -2569,7 +2577,7 @@ class DownloadManager {
         .then(() => {
           // If file has no extension, detect it from magic header and rename
           const currentExt = path.extname(download.tempName);
-          if (currentExt === "") {
+          if (currentExt === "" && !download.error) {
             log(
               "info",
               "download has no extension, detecting from magic header",
