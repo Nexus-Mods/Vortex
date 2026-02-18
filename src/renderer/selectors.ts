4c8a0b054129b988646288e6d6568a62c38a27e4
Vitalii Mikhailov <personal@aragas.org>
2026-02-18 16:05:23 +0200

    Formatting

--- src/renderer/selectors.ts
+++ src/renderer/selectors.ts
@@ -1,7 +1,8 @@
 import type { IState } from "./types/IState";
 
 export const mainPage = (state: IState) => state.session.base.mainPage;
-export const secondaryPage = (state: IState) => state.session.base.secondaryPage;
+export const secondaryPage = (state: IState) =>
+  state.session.base.secondaryPage;
 
 export const notifications = (state: IState) =>
   state.session.notifications.notifications;
