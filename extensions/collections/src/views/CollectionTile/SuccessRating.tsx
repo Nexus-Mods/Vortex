927080ea8029fc7e7e2efee315c4122e94d5ad98
IDCs <adrianraer@gmail.com>
2026-03-02 05:59:24 +0000

    minor fix for a react render warning

--- extensions/collections/src/views/CollectionTile/SuccessRating.tsx
+++ extensions/collections/src/views/CollectionTile/SuccessRating.tsx
@@ -17,9 +17,11 @@ export function SuccessRating(props: ISuccessRatingProps) {
   const [rating, setRating] = React.useState(undefined);
 
   React.useEffect(() => {
+    let mounted = true;
     (async () => {
       try {
         const rev = await infoCache.getRevisionInfo(revisionId, collectionSlug, revisionNumber);
+        if (!mounted) return;
         if ((rev?.rating?.total ?? 0) < 3) {
           setRating(undefined);
         } else {
@@ -32,6 +34,7 @@ export function SuccessRating(props: ISuccessRatingProps) {
         });
       }
     })();
+    return () => { mounted = false; };
   }, [revisionId]);
 
   const classes = [
