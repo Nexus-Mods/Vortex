f42fc3dc2999cf5b5873800c1ec8f12218aabe10
Timothy Baldridge <tbaldridge@gmail.com>
2026-03-18 14:54:35 -0600

    feat: add barrel export for model layer public API

---
+++ src/main/src/store/models.ts
@@ -0,0 +1,5 @@
+export { View } from "./View";
+export { Table } from "./Table";
+export { Database } from "./Database";
+export type { TransactionContext } from "./Database";
+export { getDatabase } from "./mainPersistence";
