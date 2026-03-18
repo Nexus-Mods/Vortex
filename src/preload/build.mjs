2475082ac03d7a85c26e3f423a674bde05003c3f
erri120 <erri120@fossmailer.de>
2026-03-18 10:50:09 +0100

    Use cjs extension for preload script

--- src/preload/build.mjs
+++ src/preload/build.mjs
@@ -4,7 +4,7 @@ import { rolldown } from "rolldown";
 import { createConfig, mainOutputDirectory } from "../../rolldown.base.mjs";
 
 const INPUT = path.resolve(import.meta.dirname, "src", "index.ts");
-const OUTPUT = path.join(mainOutputDirectory, "preload.js");
+const OUTPUT = path.join(mainOutputDirectory, "preload.cjs");
 
 const config = createConfig(INPUT, OUTPUT, "cjs", [], ["electron"]);
 
