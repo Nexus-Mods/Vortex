596d1550b8d89d89875d7f3aa2393b36aaa6ce2b
erri120 <erri120@fossmailer.de>
2026-03-24 10:31:23 +0100

    Remove barrel file

--- src/main/src/store/models.ts
+++
@@ -1,5 +0,0 @@
-export { View } from "./View";
-export { Table } from "./Table";
-export { Database } from "./Database";
-export type { TransactionContext } from "./Database";
-export { getDatabase } from "./mainPersistence";
