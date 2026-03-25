564670decb20342c2c2458f6ca00a2cce7f632ac
IDCs <adrianraer@gmail.com>
2026-03-25 15:28:59 +0000

    unresolved conflicts dialog redundant conflict entry de-duplication
    also made sure we don't dispatch rule changes from within the dialog to
    avoid having the component drop changes when unlocking rules.
    
    fixes https://linear.app/nexus-mods/issue/APP-145/unresolved-conflicts-popup-omits-mods-and-clears-unsaved-rules
    fixes https://github.com/Nexus-Mods/Vortex/issues/21861

--- extensions/mod-dependency-manager/src/views/ConflictEditor.tsx
+++ extensions/mod-dependency-manager/src/views/ConflictEditor.tsx
@@ -57,7 +57,6 @@ interface IConnectedProps {
 interface IActionProps {
   onClose: () => void;
   onAddRule: (gameId: string, modId: string, rule: any) => void;
-  onRemoveRule: (gameId: string, modId: string, rule: any) => void;
   onOverrideDialog: (gameId: string, modId: string) => void;
   onBatchDispatch: (actions: Redux.Action[]) => void;
 }
@@ -944,33 +943,10 @@ class ConflictEditor extends ComponentEx<IProps, IComponentState> {
   }
 
   private unlock = (evt: React.MouseEvent<any>) => {
-    const { t, gameId, mods, onRemoveRule } = this.props;
+    const { t } = this.props;
     const modId = evt.currentTarget.getAttribute("data-modid");
     const rule = JSON.parse(evt.currentTarget.getAttribute("data-rule"));
-    // rule is the "reverse" rule, we need the original.
-
-    const reverseType = rule.type === "before" ? "after" : "before";
-
-    const findRule = (iter) =>
-      iter.type === reverseType &&
-      util.testModReference(mods[modId], iter.reference);
-
-    const refMod: types.IMod = Object.keys(mods)
-      .map((iter) => mods[iter])
-      .find(
-        (iter) =>
-          util.testModReference(iter, rule.reference) &&
-          iter.rules !== undefined &&
-          iter.rules.find(findRule) !== undefined,
-      );
-
-    if (refMod === undefined) {
-      // paranoia check, this should not be possible. The only way it could happen if, due to a
-      // failed update we have the "reverse" rule but the original is gone.
-      return;
-    }
-
-    const originalRule = refMod.rules.find(findRule);
+    const refId = rule.reference.id;
 
     this.context.api.showDialog(
       "question",
@@ -986,10 +962,13 @@ class ConflictEditor extends ComponentEx<IProps, IComponentState> {
           label: "Remove Rule",
           default: true,
           action: () => {
-            onRemoveRule(gameId, refMod.id, {
-              type: originalRule.type,
-              reference: originalRule.reference,
-            });
+            this.nextState.rules[refId] = {
+              ...this.nextState.rules[refId],
+              [modId]: {
+                type: undefined,
+                version: "any",
+              },
+            };
           },
         },
       ],
@@ -1117,8 +1096,6 @@ function mapDispatchToProps(
     onClose: () => dispatch(setConflictDialog(undefined, undefined, undefined)),
     onAddRule: (gameId, modId, rule) =>
       dispatch(vortexActions.addModRule(gameId, modId, rule)),
-    onRemoveRule: (gameId, modId, rule) =>
-      dispatch(vortexActions.removeModRule(gameId, modId, rule)),
     onOverrideDialog: (gameId: string, modId: string) =>
       dispatch(setFileOverrideDialog(gameId, modId)),
     onBatchDispatch: (actions: Redux.Action[]) => dispatch(batch(actions)),
