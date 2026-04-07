52f9349410c6432098e19b7d5813dc133cb4da1e
erri120 <erri120@fossmailer.de>
2026-04-07 09:13:50 +0200

    Remove deprecated paths packages

--- packages/paths-node/src/index.ts
+++
@@ -1,5 +0,0 @@
-// Export Node.js-specific implementations
-export { NodeFilesystem } from "./NodeFilesystem";
-
-// Re-export everything from @vortex/paths for convenience
-export * from "@vortex/paths";
