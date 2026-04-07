731ec68d396552a6b2f1d5a0dc0e64019e04931b
erri120 <erri120@fossmailer.de>
2026-04-07 11:53:39 +0200

    Cleanup ping-test adaptor

--- packages/adaptors/ping-test/tsdown.config.ts
+++ packages/adaptors/ping-test/tsdown.config.ts
@@ -2,18 +2,13 @@ import { defineConfig } from "tsdown";
 import { vortexAdaptorPlugin } from "@vortex/adaptor-api/plugin";
 
 export default defineConfig({
-  entry: {
-    index: "./src/index.ts",
-  },
+  entry: "./src/index.ts",
   format: ["esm", "commonjs"],
   dts: {
     sourcemap: true,
   },
   exports: true,
   platform: "neutral",
-  deps: {
-    neverBundle: [/^@vortex\/adaptor-api/],
-  },
   plugins: [
     vortexAdaptorPlugin({
       ping: "vortex:host/ping",
