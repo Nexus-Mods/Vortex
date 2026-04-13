68dc2f8a27bc8d63d642774f58259ca0946c9bce
erri120 <erri120@fossmailer.de>
2026-04-13 16:41:43 +0200

    Update windows-specific base

--- packages/fs/src/browser/paths.windows.ts
+++ packages/fs/src/browser/paths.windows.ts
@@ -1,7 +1,21 @@
 import type { OSPathBase, PathProvider, QualifiedPath } from "./paths";
 
+/**
+ * Bases supported by all OS path providers.
+ * @public */
+export const WindowsPath = {
+  /** `%USERPROFILE%/AppData` */
+  appData: "appData",
+  /** `%USERPROFILE%/Documents` also known as "My Documents" */
+  documents: "documents",
+  /** `%USERPROFILE%/Documents/My Games` */
+  myGames: "my games",
+} as const;
+
 /** @public */
-export type WindowsPathBase = OSPathBase;
+export type WindowsPathBase =
+  | OSPathBase
+  | (typeof WindowsPath)[keyof typeof WindowsPath];
 
 /** @public */
 export interface WindowsPathProvider extends PathProvider<WindowsPathBase> {
