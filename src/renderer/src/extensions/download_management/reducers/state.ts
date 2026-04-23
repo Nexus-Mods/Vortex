c26d11161c75fb78ce9b87495dfd8727e2a92993
erri120 <erri120@fossmailer.de>
2026-04-23 15:37:52 +0200

    Fix array slice

--- src/renderer/src/extensions/download_management/reducers/state.ts
+++ src/renderer/src/extensions/download_management/reducers/state.ts
@@ -198,7 +198,7 @@ export const stateReducer: IReducerSpec = {
     [action.setDownloadSpeed as any]: (state, payload) => {
       const temp = setSafe(state, ["speed"], payload);
       let speeds =
-        state.speedHistory !== undefined ? state.speedHistory.slice() : [];
+        Array.isArray(state.speedHistory) ? state.speedHistory.slice() : [];
       speeds.push(payload);
       if (speeds.length > NUM_SPEED_DATA_POINTS) {
         speeds = speeds.slice(speeds.length - NUM_SPEED_DATA_POINTS);
