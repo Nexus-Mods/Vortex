40175eae820382d9f0b43d3ae16a0c0c216685df
Mike <pickysaurus@googlemail.com>
2026-05-16 11:58:27 +0100

    Enable ESL support in Fallout 4 VR
    Fixes #18789
    
    Adds parity with Skyrim VR.

--- extensions/games/game-fallout4vr/src/index.js
+++ extensions/games/game-fallout4vr/src/index.js
@@ -4,6 +4,10 @@ const { getFileVersion } = require("exe-version");
 const { util } = require("vortex-api");
 const winapi = require("winapi-bindings");
 
+const GAME_ID = "fallout4vr";
+const ESL_ENABLER_LIB = "Daytripper4.dll";
+const ESL_NOTIF_ID = "fallout4vr-esl-enabler-notif";
+
 /*
 Ignore the Meshes\AnimTextData\AnimationOffsets\PersistantSubgraphInfoAndOffsetData.txt file as a conflict. 
 It's present in a lot of weapon mods but doesn't matter if it's overwritten. 
@@ -34,7 +38,7 @@ function getGameVersion(gamePath, exePath) {
   return fileVersion + "-VR";
 }
 
-let tools = [
+const tools = [
   {
     id: "FO4VREdit",
     name: "FO4VREdit",
@@ -51,9 +55,77 @@ let tools = [
   },
 ];
 
+function isESLSupported(api) {
+  const state = api.getState();
+  const profileId = selectors.lastActiveProfileForGame(state, GAME_ID);
+  const discovery = selectors.discoveryByGame(state, GAME_ID);
+  if (discovery?.store === "xbox") {
+    return false;
+  }
+  const modState = util.getSafe(state, ["persistent", "profiles", profileId, "modState"], {});
+  const isEnabled = (modId) => util.getSafe(modState, [modId, "enabled"], false);
+  const mods = util.getSafe(state, ["persistent", "mods", GAME_ID], {});
+  const hasESLEnabler = Object.keys(mods).some(
+    (modId) => isEnabled(modId) && mods[modId]?.attributes?.eslEnabler === true,
+  );
+  if (hasESLEnabler) {
+    api.dismissNotification(ESL_NOTIF_ID);
+  }
+  return hasESLEnabler;
+}
+
+function testEslEnabler(files, gameId) {
+  const isFallout4VR = gameId === GAME_ID;
+  const isESLEnabler = files.some((file) => file.toLowerCase().endsWith(ESL_ENABLER_LIB));
+  return Promise.resolve({
+    supported: isFallout4VR && isESLEnabler,
+    requiredFiles: [],
+  });
+}
+
+function installEslEnabler(files, destinationPath) {
+  const filtered = files.filter((file) => path.extname(file) !== "");
+  const instructions = filtered.map((file) => {
+    const segments = file.split(path.sep);
+    segments.splice(0, 1, "Data");
+    return {
+      type: "copy",
+      source: file,
+      destination: segments.join(path.sep),
+    };
+  });
+
+  // Remove this once the mod type conflict issue is resolved
+  instructions.push({ type: "setmodtype", value: "dinput" });
+  instructions.push({ type: "attribute", key: "eslEnabler", value: true });
+
+  return Promise.resolve({ instructions });
+}
+
+function prepare(api, discovery) {
+  if (isESLSupported(api)) {
+    return Promise.resolve();
+  }
+
+  api.sendNotification({
+    id: ESL_NOTIF_ID,
+    type: "info",
+    title: "ESL Support",
+    message:
+      "Fallout 4 VR requires a mod to enable ESL support. Mod must be installed through Vortex for ESL support to work.",
+    actions: [
+      {
+        title: "Download",
+        action: () =>
+          util.opn("https://www.nexusmods.com/fallout4/mods/91141?tab=files").catch(() => {}),
+      },
+    ],
+  });
+}
+
 function main(context) {
   context.registerGame({
-    id: "fallout4vr",
+    id: GAME_ID,
     name: "Fallout 4 VR",
     mergeMods: true,
     queryPath: findGame,
@@ -62,6 +134,7 @@ function main(context) {
     logo: "gameart.jpg",
     executable: () => "Fallout4VR.exe",
     getGameVersion,
+    setup: (discovery) => prepare(context.api, discovery),
     requiredFiles: ["Fallout4VR.exe"],
     environment: {
       SteamAPPId: "611660",
@@ -69,10 +142,15 @@ function main(context) {
     details: {
       steamAppId: 611660,
       compatibleDownloads: ["fallout4"],
+      supportsESL: () => isESLSupported(context.api),
       ignoreConflicts: IGNORED_FILES,
       nexusPageId: "fallout4",
     },
   });
+
+  context.registerInstaller("fallout4vr-esl-enabler", 10, testEslEnabler, installEslEnabler);
+
+  context.registerTest("fallout4vr-esl-support-missing", "did-deploy", () => {});
 
   return true;
 }
