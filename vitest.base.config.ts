c72b567c27d664c94a76b092e3e2d46ca8401e53
erri120 <erri120@fossmailer.de>
2026-05-21 11:31:19 +0200

    Remove test output file

--- vitest.base.config.ts
+++ vitest.base.config.ts
@@ -1,8 +1,5 @@
-import * as path from "node:path";
-
 import { defineConfig } from "vitest/config";
 
-const RESULTS_DIR = path.join(import.meta.dirname, "test-results");
 const isGitHubCI = process.env.CI && process.env.GITHUB_ACTIONS;
 
 export default defineConfig({
@@ -10,13 +7,5 @@ export default defineConfig({
     reporters: ["default", "junit", isGitHubCI ? "github-actions" : undefined].filter(
       Boolean,
     ) as string[],
-    outputFile: {
-      junit: path.join(RESULTS_DIR, "junit.xml"),
-    },
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "cobertura"],
-      reportsDirectory: RESULTS_DIR,
-    },
   },
 });
