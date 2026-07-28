a962709504a3eaf198e3abc03b45d0ef1845a634
erri120 <erri120@fossmailer.de>
2026-07-28 15:54:24 +0200

    Fix removing extensions

--- src/renderer/src/extensions/extension_manager/ExtensionManager.tsx
+++ src/renderer/src/extensions/extension_manager/ExtensionManager.tsx
@@ -330,8 +330,7 @@ class ExtensionManager extends ComponentEx<IProps, IComponentState> {
 
   private removeExtension = (extIds: string[]) => {
     extIds.forEach((extId) => {
-      const ext = this.props.extensions[extId];
-      this.props.onRemoveExtension(path.basename(ext.path));
+      this.props.onRemoveExtension(extId);
     });
   };
 }
